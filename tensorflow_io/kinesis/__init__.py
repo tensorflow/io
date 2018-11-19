bbf305f04a26fc2022a3549e6066130c9b395b8c
Yong Tang <yong.tang.github@outlook.com>
2018-11-19 02:47:59 +0000

    Rework on tensorflow_io/kinesis/__init__.py
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/kinesis/__init__.py
+++ tensorflow_io/kinesis/__init__.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.contrib.kinesis.python.ops.kinesis_dataset_ops import KinesisDataset
+from tensorflow_io.kinesis.python.ops.kinesis_dataset_ops import KinesisDataset
 
 from tensorflow.python.util.all_util import remove_undocumented
 
