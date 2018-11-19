e37a087b509d79c90ef23688f30e3178644591d8
Yong Tang <yong.tang.github@outlook.com>
2018-11-19 01:37:08 +0000

    Rework on tensorflow_io/kafka/__init__.py
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/kafka/__init__.py
+++ tensorflow_io/kafka/__init__.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.contrib.kafka.python.ops.kafka_dataset_ops import KafkaDataset
+from tensorflow_io.kafka.python.ops.kafka_dataset_ops import KafkaDataset
 
 from tensorflow.python.util.all_util import remove_undocumented
 
