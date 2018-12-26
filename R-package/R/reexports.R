5681e776d8c543a316f6b47f7f221e7489e169b8
terrytangyuan <terrytangyuan@gmail.com>
2018-12-26 18:42:27 -0500

    Re-exports reusable methods from tfdatasets package

--- R-package/R/reexports.R
+++ R-package/R/reexports.R
@@ -25,3 +25,97 @@ tensorflow::tf_config
 #' @importFrom tensorflow tf_version
 #' @export
 tensorflow::tf_version
+
+
+# Re-exports from tfdatasets dataset_iterators
+
+#' @importFrom tfdatasets next_batch
+#' @export
+tfdatasets::next_batch
+
+#' @importFrom tfdatasets with_dataset
+#' @export
+tfdatasets::with_dataset
+
+#' @importFrom tfdatasets until_out_of_range
+#' @export
+tfdatasets::until_out_of_range
+
+
+# Re-exports from tfdatasets dataset_methods
+
+#' @importFrom tfdatasets dataset_repeat
+#' @export
+tfdatasets::dataset_repeat
+
+#' @importFrom tfdatasets dataset_shuffle
+#' @export
+tfdatasets::dataset_shuffle
+
+#' @importFrom tfdatasets dataset_shuffle_and_repeat
+#' @export
+tfdatasets::dataset_shuffle_and_repeat
+
+#' @importFrom tfdatasets dataset_batch
+#' @export
+tfdatasets::dataset_batch
+
+#' @importFrom tfdatasets dataset_cache
+#' @export
+tfdatasets::dataset_cache
+
+#' @importFrom tfdatasets dataset_concatenate
+#' @export
+tfdatasets::dataset_concatenate
+
+#' @importFrom tfdatasets dataset_take
+#' @export
+tfdatasets::dataset_take
+
+#' @importFrom tfdatasets dataset_map
+#' @export
+tfdatasets::dataset_map
+
+#' @importFrom tfdatasets dataset_map_and_batch
+#' @export
+tfdatasets::dataset_map_and_batch
+
+#' @importFrom tfdatasets dataset_flat_map
+#' @export
+tfdatasets::dataset_flat_map
+
+#' @importFrom tfdatasets dataset_prefetch
+#' @export
+tfdatasets::dataset_prefetch
+
+#' @importFrom tfdatasets dataset_prefetch_to_device
+#' @export
+tfdatasets::dataset_prefetch_to_device
+
+#' @importFrom tfdatasets dataset_filter
+#' @export
+tfdatasets::dataset_filter
+
+#' @importFrom tfdatasets dataset_skip
+#' @export
+tfdatasets::dataset_skip
+
+#' @importFrom tfdatasets dataset_interleave
+#' @export
+tfdatasets::dataset_interleave
+
+#' @importFrom tfdatasets dataset_prefetch
+#' @export
+tfdatasets::dataset_prefetch
+
+#' @importFrom tfdatasets dataset_shard
+#' @export
+tfdatasets::dataset_shard
+
+#' @importFrom tfdatasets dataset_padded_batch
+#' @export
+tfdatasets::dataset_padded_batch
+
+#' @importFrom tfdatasets dataset_prepare
+#' @export
+tfdatasets::dataset_prepare
