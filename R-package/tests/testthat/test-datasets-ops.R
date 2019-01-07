32d27c14ef360716919f775e2e492fc0aff29e38
terrytangyuan <terrytangyuan@gmail.com>
2019-01-06 19:39:44 -0500

    Added simple testcase for sequence_file_dataset()

--- R-package/tests/testthat/test-datasets-ops.R
+++ R-package/tests/testthat/test-datasets-ops.R
@@ -2,6 +2,7 @@ context("TensorFlow IO dataset ops")
 
 source("utils.R")
 
-test_succeeds("All TensorFlow IO dataset ops work", {
-  print("Placeholder for now")
+test_succeeds("sequence_file_dataset() works successfully", {
+  sequence_file_dataset("testdata/string.seq") %>%
+    dataset_repeat(2)
 })
