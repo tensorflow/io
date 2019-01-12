b989d0365d28bf73285505bd569e1012aabc33cb
terrytangyuan <terrytangyuan@gmail.com>
2019-01-12 17:08:23 -0500

    Added parquet_dataset()

--- R-package/tests/testthat/test-datasets-ops.R
+++ R-package/tests/testthat/test-datasets-ops.R
@@ -24,3 +24,17 @@ test_succeeds("ignite_dataset() works successfully", {
   repeat_dataset = ignite_dataset(
       cache_name = "SQL_PUBLIC_TEST_CACHE", port = 42300)
 })
+
+test_succeeds("parquet_dataset() works successfully", {
+  dtypes <- tf$python$framework$dtypes
+  constant_op <- tf$python$constant_op
+  filenames <- constant_op$constant(
+    list("testdata/parquet_cpp_example.parquet"), dtypes$string)
+  columns <- list(0, 1, 2, 4, 5)
+  output_types <- reticulate::tuple(
+    dtypes$bool, dtypes$int32, dtypes$int64, dtypes$float32, dtypes$float64)
+  num_repeats <- 2
+
+  parquet_dataset(filenames, columns, output_types) %>%
+    dataset_repeat(num_repeats)
+})
