0f3011a3431eeb1b14de6816161b5067ac7f3715
Yong Tang <yong.tang.github@outlook.com>
2019-02-17 03:30:38 +0000

    Test Ubuntu 16.04 and 18.04 for install and R
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .travis/install.test.sh
+++ .travis/install.test.sh
@@ -18,19 +18,26 @@ set -e -x
 # Install needed repo
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq update
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev \
-    software-properties-common \
+    software-properties-common apt-transport-https \
     python-pip python3-pip \
-    python-pytest python3-pytest \
     ffmpeg > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 DEBIAN_FRONTEND=noninteractive add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran35/"
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq update
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install r-base > /dev/null
 echo "options(repos = c(CRAN='http://cran.rstudio.com'))" >> ~/.Rprofile
-R -e 'install.packages(c("Rcpp", "reticulate", "knitr", "tensorflow", "tfdatasets", "forge", "tidyselect"), quiet = TRUE)'
+R -e 'install.packages(c("tensorflow", "tfdatasets"), quiet = TRUE)'
 R -e 'install.packages(c("testthat", "devtools"), quiet = TRUE)'
-python3 -m pip install dist/*36*.whl
-python3 -m pip install boto3 pyarrow==0.11.1 pandas==0.19.2
-python -m pip install dist/*27*.whl
-python -m pip install boto3 pyarrow==0.11.1 pandas==0.19.2
-(cd tests && python -m pytest -s .)
+R -e 'install.packages(c("forge"), quiet = TRUE)'
+
+V=$(python3 -c 'import sys; print(str(sys.version_info[0])+str(sys.version_info[1]))')
+python3 -m pip install dist/*${V}*.whl
+python3 -m pip install -q pytest boto3 pyarrow==0.11.1 pandas==0.19.2
+(cd tests && python3 -m pytest .)
+
+V=$(python -c 'import sys; print(str(sys.version_info[0])+str(sys.version_info[1]))')
+python -m pip install dist/*${V}*.whl
+python -m pip install -q pytest boto3 pyarrow==0.11.1 pandas==0.19.2
+(cd tests && python -m pytest .)
+
+(cd R-package && R -e 'stopifnot(all(data.frame(devtools::test())$failed == 0L))')
