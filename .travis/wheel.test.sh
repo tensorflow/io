5f9112b0b5e072427f6b1dbbf509775bab457579
Yong Tang <yong.tang.github@outlook.com>
2019-02-26 05:09:15 +0000

    Add mising R dependency, and improve the test condition.
    Encountered a scneario where all tests are skipped
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .travis/wheel.test.sh
+++ .travis/wheel.test.sh
@@ -29,7 +29,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -qq install software-properties-common
 DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 DEBIAN_FRONTEND=noninteractive add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran35/"
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq update
-# Note: libpython-dev libpython3-dev are needed to for reticulate to see python binding
+# Note: libpython-dev libpython3-dev are needed to for reticulate to see python binding (/usr/lib/libpython2.7.so)
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install libpython-dev libpython3-dev > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install r-base > /dev/null
 echo "options(repos = c(CRAN='http://cran.rstudio.com'))" >> ~/.Rprofile
@@ -38,4 +38,4 @@ R -e 'install.packages(c("testthat", "devtools"), quiet = TRUE)'
 R -e 'install.packages(c("forge"), quiet = TRUE)'
 R -e 'library("devtools"); install_github("rstudio/tfdatasets", ref="c6fc59b", quiet = TRUE)'
 
-(cd R-package && R -e 'stopifnot(all(data.frame(devtools::test())$failed == 0L))')
+(cd R-package && R -e 'v <- data.frame(devtools::test()); stopifnot(all(!v$failed && !v$skipped && !v$error))')
