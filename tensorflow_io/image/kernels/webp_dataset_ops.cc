fc1f044e0bf2e51c0996ba5551d0b65be5d005e6
Stephan Uphoff <ups@stups.com>
2019-03-01 17:06:09 -0700

    Decode WebP directly into tensor buffer.

--- tensorflow_io/image/kernels/webp_dataset_ops.cc
+++ tensorflow_io/image/kernels/webp_dataset_ops.cc
@@ -93,10 +93,10 @@ class WebPDatasetOp : public DatasetOpKernel {
                              std::vector<Tensor>* out_tensors,
                              bool* end_of_sequence) override {
         mutex_lock l(mu_);
-	if (current_file_index_ == 0) {
+        if (current_file_index_ == 0) {
           TF_RETURN_IF_ERROR(SetupStreamsLocked(ctx->env()));
-	}
-	if (current_file_index_ < dataset()->filenames_.size()) {
+        }
+        if (current_file_index_ < dataset()->filenames_.size()) {
           const string& filename = dataset()->filenames_[current_file_index_];
           uint64 size = 0;
           TF_RETURN_IF_ERROR(ctx->env()->GetFileSize(filename, &size));
@@ -107,19 +107,29 @@ class WebPDatasetOp : public DatasetOpKernel {
           TF_RETURN_IF_ERROR(stream->ReadNBytes(size, &data));
           WebPDecoderConfig config;
           WebPInitDecoderConfig(&config);
+          int returned = WebPGetFeatures(reinterpret_cast<const uint8_t *>(data.c_str()),
+                                         size, &config.input);
+          if (returned != VP8_STATUS_OK) {
+            return errors::InvalidArgument("File could not be decoded as WebP: ", filename);
+          }
           // TODO (yongtang): Set channel = 4 for now.
-          config.output.colorspace = MODE_RGBA;
           static const int channel = 4;
-          int returned = DecodeWebP(reinterpret_cast<const uint8_t *>(data.c_str()), size, &config);
-          Tensor value_tensor(ctx->allocator({}), DT_UINT8, {config.output.height, config.output.width, channel});
-          const uint64_t num_bytes = channel * config.output.width * (uint64_t)config.output.height;
-          std::memcpy(reinterpret_cast<char*>(value_tensor.flat<uint8_t>().data()), config.output.u.RGBA.rgba, num_bytes * sizeof(uint8_t));
+          Tensor value_tensor(ctx->allocator({}), DT_UINT8, {config.input.height, config.input.width, channel});
+
+          config.output.colorspace = MODE_RGBA;
+          config.output.u.RGBA.rgba = value_tensor.flat<uint8_t>().data();
+          config.output.u.RGBA.stride = config.input.width * channel;
+          config.output.u.RGBA.size = config.input.height * config.input.width * channel;
+          config.output.is_external_memory = 1;
+          returned = WebPDecode(reinterpret_cast<const uint8_t *>(data.c_str()), size, &config);
+          if (returned != VP8_STATUS_OK) {
+            return errors::InvalidArgument("File could not be decoded as WebP: ", filename);
+          }
           out_tensors->emplace_back(std::move(value_tensor));
-          WebPFreeDecBuffer(&config.output);
           *end_of_sequence = false;
           ++current_file_index_;
-	  return Status::OK();
-	}
+          return Status::OK();
+        }
         *end_of_sequence = true;
         return Status::OK();
       }
@@ -170,21 +180,25 @@ class DecodeWebPOp : public OpKernel {
 
     WebPDecoderConfig config;
     WebPInitDecoderConfig(&config);
+    int returned = WebPGetFeatures(reinterpret_cast<const uint8_t *>(contents.data()), contents.size(), &config.input);
+    OP_REQUIRES(context, returned == VP8_STATUS_OK,
+                errors::InvalidArgument("contents could not be decoded as WebP: ", returned));
+
+    int height = config.input.height;
+    int width = config.input.width;
+
+    Tensor* output_tensor = nullptr;
+    OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({height, width, channels_}), &output_tensor));
+
     config.output.colorspace = MODE_RGBA;
-    int returned = DecodeWebP(reinterpret_cast<const uint8_t *>(contents.data()), contents.size(), &config);
+    config.output.u.RGBA.rgba = output_tensor->flat<uint8_t>().data();
+    config.output.u.RGBA.stride = width * channels_;
+    config.output.u.RGBA.size = height * width * channels_;
+    config.output.is_external_memory = 1;
+
+    returned = DecodeWebP(reinterpret_cast<const uint8_t *>(contents.data()), contents.size(), &config);
     OP_REQUIRES(context, returned == 0,
                 errors::InvalidArgument("contents could not be decoded as WebP: ", returned));
-    int height = config.output.height;
-    int width = config.output.width;
-    int num_bytes = height * width * channels_;
-    // Allocate tensor
-    Tensor* output_tensor = nullptr;
-    const auto status = context->allocate_output(0, TensorShape({height, width, channels_}), &output_tensor);
-    if (status.ok()) {
-      std::memcpy(reinterpret_cast<char*>(output_tensor->flat<uint8_t>().data()), config.output.u.RGBA.rgba, num_bytes * sizeof(uint8_t));
-    }
-    WebPFreeDecBuffer(&config.output);
-    OP_REQUIRES_OK(context, status);
   }
 
  private:
