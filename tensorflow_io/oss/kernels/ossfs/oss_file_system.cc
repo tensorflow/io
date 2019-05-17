7b6eef0b3a2fd9d2cd7d328403092ffdcf1a1f81
zhengke.gzk <zhengke.gzk@alibaba-inc.com>
2019-05-17 21:43:47 +0800

    update code

--- tensorflow_io/oss/kernels/ossfs/oss_file_system.cc
+++ tensorflow_io/oss/kernels/ossfs/oss_file_system.cc
@@ -531,7 +531,6 @@ Status OSSFileSystem::_ParseOSSURIPath(const StringPiece fname,
 
   // contains id, key, host information
   size_t pos = bucketp.find(bucketDelim);
-  size_t tmpPos = pos;
   bucket = string(bucketp.substr(0, pos));
   StringPiece access_info = bucketp.substr(pos+1);
   std::vector<std::string> access_infos = str_util::Split(access_info, accessDelim);
@@ -559,38 +558,6 @@ Status OSSFileSystem::_ParseOSSURIPath(const StringPiece fname,
   if (bucket.empty()) {
     return errors::InvalidArgument("OSS path does not contain a bucket name:", fname);
   }
-
-//  } else if (bucketp.find('?') != StringPiece::npos) {
-//    // contains id, key, host information
-//    size_t pos = bucketp.find('?');
-//    bucket = string(bucketp.substr(0, pos));
-//    StringPiece access_info = bucketp.substr(pos+1);
-//    std::vector<std::string> access_infos = str_util::Split(access_info, "&");
-//    for (const auto& key_value : access_infos) {
-//      StringPiece data(key_value);
-//      size_t pos = data.find('=');
-//      if (pos == StringPiece::npos) {
-//        return errors::InvalidArgument("OSS path access info faied: ",
-//          fname, " info:", key_value);
-//      }
-//      StringPiece key = data.substr(0, pos);
-//      StringPiece value = data.substr(pos + 1);
-//      if (str_util::StartsWith(key, kOSSAccessIdKey)) {
-//        access_id = string(value);
-//      } else if (str_util::StartsWith(key, kOSSAccessKeyKey)) {
-//        access_key = string(value);
-//      } else if (str_util::StartsWith(key, kOSSHostKey)) {
-//        host = string(value);
-//      } else {
-//        return errors::InvalidArgument("OSS path access info faied: ",
-//          fname, " unkown info:", key_value);
-//      }
-//    }
-//
-//    if (bucket.empty()) {
-//      return errors::InvalidArgument("OSS path does not contain a bucket name:", fname);
-//    }
-//  }
 
   if (access_id.empty() || access_key.empty() || host.empty()) {
     return errors::InvalidArgument("OSS path does not contain valid access info:", fname);
