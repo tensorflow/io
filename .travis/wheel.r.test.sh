63d5773f24a5498113e683d933e8cb5fd0d60b67
Yong Tang <yong.tang.github@outlook.com>
2019-06-03 21:01:27 +0000

    CI Setup with kokoro:run
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .travis/wheel.r.test.sh
+++ .travis/wheel.r.test.sh
@@ -9,6 +9,7 @@ CPYTHON_VERSION=$(python -c 'import sys; print(str(sys.version_info[0])+str(sys.
 (cd wheelhouse && python -m pip install *-cp${CPYTHON_VERSION}-*.whl)
 python -m pip install -q pytest boto3 google-cloud-pubsub==0.39.1 pyarrow==0.11.1 pandas==0.19.2
 
+set +e
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install software-properties-common apt-transport-https
 DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 DEBIAN_FRONTEND=noninteractive add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran35/"
@@ -16,6 +17,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -qq update
 # Note: libpython-dev libpython3-dev are needed to for reticulate to see python binding (/usr/lib/libpython2.7.so)
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install libpython-dev libpython3-dev
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install r-base
+set -e
 echo "options(repos = c(CRAN='http://cran.rstudio.com'))" >> ~/.Rprofile
 R -e 'install.packages(c("tensorflow"), quiet = TRUE)'
 R -e 'install.packages(c("testthat", "devtools"), quiet = TRUE)'
