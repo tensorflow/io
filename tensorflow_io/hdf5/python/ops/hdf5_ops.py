0160352cfe43b43b3e961f9c253404d36e06aea8
Yong Tang <yong.tang.github@outlook.com>
2019-06-08 13:59:02 -0700

    Update dependency to 1.14.0rc0 and 2.0.0beta (#279)
    * Update dependency to 1.14.0rc0 and 2.0.0beta
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Temporarily link to libtensorflow_framework.so.1
    
    Seems 2.0.0beta still use libtensorflow_framework.so.1
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Change tensorflow -> tf, and change lib for mac
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/hdf5/python/ops/hdf5_ops.py
+++ tensorflow_io/hdf5/python/ops/hdf5_ops.py
@@ -58,7 +58,7 @@ class HDF5Dataset(data.Dataset):
 
   @property
   def output_shapes(self):
-    return tuple([shape for shape in self._shapes])
+    return tuple([tf.TensorShape([]) for _ in self._shapes])
 
   @property
   def output_types(self):
