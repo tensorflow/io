f27e4a764fdde79e2e86b38ecebc3ec3f2900bfe
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2019-07-03 16:39:36 -0700

    properly adding DGRPC_BAZEL_BUILD to travis build rule for MacOS

--- .travis/bazel.build.sh
+++ .travis/bazel.build.sh
@@ -4,7 +4,7 @@ if [[ $(uname) == "Darwin" ]]; then
 # The -DGRPC_BAZEL_BUILD is needed because gRPC does not compile on macOS unless
 # it is set.
   bazel build \
-    --copt=-DGRPC_BAZEL_BUILD
+    --copt=-DGRPC_BAZEL_BUILD \
     --noshow_progress \
     --noshow_loading_progress \
     --verbose_failures \
