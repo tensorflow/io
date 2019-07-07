9643e79d15e1cffa7421761c2b7b92367e6ee0ec
Damien Pontifex <damien.pontifex@gmail.com>
2019-07-07 11:28:27 +1000

    skip linux tests for now

--- tests/test_azure/start_azure.sh
+++ tests/test_azure/start_azure.sh
@@ -1,8 +1,13 @@
 set -e
 set -o pipefail
 
-npm install -g azurite@2.7.0
-echo starting azurite-blob
-azurite-blob &
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  npm install -g azurite@2.7.0
+  echo starting azurite-blob
+  azurite-blob &
+else
+  docker run -t -d --rm --net=host node bash -c 'npm install -g azurite@2.7.0 && azurite-blob'
+fi
 sleep 10 # Wait for storage emulator to start
 echo azurite-blob started successfully
