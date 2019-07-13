de2bafa363c3c5a6510540bd97d365cfdce815c8
Russell Power <russell.power@gmail.com>
2019-07-13 06:35:07 -0700

    Add GCS configuration ops. (#339)
    * Add GCS configuration ops.
    
    * Fix pylint warnings.
    
    * Remove stray bigtable reference.
    
    * Fix TF v1 build.
    
    * Fix lint errors.

---
+++ tests/test_gcs_config_ops.py
@@ -0,0 +1,43 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for the gcs_config_ops."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from tensorflow.python.platform import test
+from tensorflow_io import gcs
+
+tf_v1 = tf.version.VERSION.startswith('1')
+
+class GcsConfigOpsTest(test.TestCase):
+  """GCS Config OPS test"""
+  def test_set_block_cache(self):
+    cfg = gcs.BlockCacheParams(max_bytes=1024*1024*1024)
+    if tf_v1:
+      with tf.Session() as session:
+        gcs.configure_gcs(session,
+                          credentials=None,
+                          block_cache=cfg,
+                          device=None)
+    else:
+      gcs.configure_gcs(block_cache=cfg)
+
+
+if __name__ == '__main__':
+  test.main()
