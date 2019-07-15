09cb9ea5e022f65f35ac4a038d3b2ef03278d71a
Yong Tang <yong.tang.github@outlook.com>
2019-07-15 03:06:35 +0000

    Add save_csv support
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/text/kernels/text_output.cc
+++ tensorflow_io/text/kernels/text_output.cc
@@ -12,87 +12,28 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow/core/framework/dataset.h"
-#include "tensorflow/core/framework/function_handle_cache.h"
-#include "tensorflow/core/framework/op_kernel.h"
-#include "tensorflow/core/lib/core/threadpool.h"
-#include "tensorflow/core/lib/io/record_writer.h"
-#include "tensorflow/core/platform/file_system.h"
+#include "kernels/output_ops.h"
 
 namespace tensorflow {
 namespace data {
 namespace {
 
-class TextDatasetOutputOp : public AsyncOpKernel {
+class TextDatasetOutput {
  public:
-  explicit TextDatasetOutputOp(OpKernelConstruction* ctx)
-      : AsyncOpKernel(ctx),
-        background_worker_(ctx->env(), "text_dataset_output") {}
-
-  template <typename T>
-  Status ParseScalarArgument(OpKernelContext* ctx,
-                             const StringPiece& argument_name, T* output) {
-    const Tensor* argument_t;
-    TF_RETURN_IF_ERROR(ctx->input(argument_name, &argument_t));
-    if (!TensorShapeUtils::IsScalar(argument_t->shape())) {
-      return errors::InvalidArgument(argument_name, " must be a scalar");
-    }
-    *output = argument_t->scalar<T>()();
-    return Status::OK();
+  Status Final(WritableFile *file) {
+    return file->Flush();
   }
-
-  void ComputeAsync(OpKernelContext* ctx, DoneCallback done) override {
-    // The call to `iterator->GetNext()` may block and depend on an inter-op
-    // thread pool thread, so we issue the call using a background thread.
-    background_worker_.Schedule([this, ctx, done]() {
-      string filename;
-      OP_REQUIRES_OK_ASYNC(
-          ctx, ParseScalarArgument<string>(ctx, "filename", &filename), done);
-      std::unique_ptr<WritableFile> file;
-      OP_REQUIRES_OK_ASYNC(ctx, ctx->env()->NewWritableFile(filename, &file),
-                           done);
-
-      DatasetBase* dataset;
-      OP_REQUIRES_OK_ASYNC(
-          ctx, GetDatasetFromVariantTensor(ctx->input(0), &dataset), done);
-      std::unique_ptr<IteratorBase> iterator;
-      IteratorContext::Params params(ctx);
-      std::unique_ptr<FunctionHandleCache> function_handle_cache =
-          absl::make_unique<FunctionHandleCache>(params.flr);
-      params.function_handle_cache = function_handle_cache.get();
-      IteratorContext iter_ctx(std::move(params));
-
-      OP_REQUIRES_OK_ASYNC(
-          ctx,
-          dataset->MakeIterator(&iter_ctx, "TextDatasetOutputOpIterator", &iterator),
-          done);
-      std::vector<Tensor> components;
-      components.reserve(dataset->output_dtypes().size());
-      bool end_of_sequence;
-      do {
-        OP_REQUIRES_OK_ASYNC(
-            ctx, iterator->GetNext(&iter_ctx, &components, &end_of_sequence),
-            done);
-
-        if (!end_of_sequence) {
-          // TODO (yongtang): In case of TextDataset the shapes of output can
-          // only be either a scalar (`batch = 0`) or an 1-D tensor (`batch != 0`)
-          // so the following could be simplified by just calling flat<string>.
-          // Once other dataset has been introduced for write then additional
-          // logic would be neededto handle `batch = 0` vs `batch != 0` case.
-          for (int64 i = 0; i < components[0].NumElements(); i++) {
-            OP_REQUIRES_OK_ASYNC(
-                ctx, file.get()->Append(components[0].flat<string>()(i)), done);
-            OP_REQUIRES_OK_ASYNC(
-                ctx, file.get()->Append("\n"), done);
-          }
-        }
-        components.clear();
-      } while (!end_of_sequence);
-          OP_REQUIRES_OK_ASYNC(
-              ctx, file.get()->Flush(), done);
-      done();
-    });
+  Status Write(WritableFile *file, std::vector<Tensor>& components) {
+    // TODO (yongtang): In case of TextDataset the shapes of output can
+    // only be either a scalar (`batch = 0`) or an 1-D tensor (`batch != 0`)
+    // so the following could be simplified by just calling flat<string>.
+    // Once other dataset has been introduced for write then additional
+    // logic would be neededto handle `batch = 0` vs `batch != 0` case.
+    for (int64 i = 0; i < components[0].NumElements(); i++) {
+      TF_RETURN_IF_ERROR(file->Append(components[0].flat<string>()(i)));
+      TF_RETURN_IF_ERROR(file->Append("\n"));
+    }
+    return Status::OK();
   }
 
  private:
@@ -100,7 +41,7 @@ class TextDatasetOutputOp : public AsyncOpKernel {
 };
 
 REGISTER_KERNEL_BUILDER(
-    Name("TextDatasetOutput").Device(DEVICE_CPU), TextDatasetOutputOp);
+    Name("TextDatasetOutput").Device(DEVICE_CPU), DatasetOutputOp<TextDatasetOutput>);
 
 }  // namespace
 }  // namespace data
