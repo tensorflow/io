47490682863219d18bf2879a9d6e0153057c7cd0
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2019-07-17 16:05:48 +0200

    Add implementation of ReadRecord on the top of jsoncpp

--- tensorflow_io/json/kernels/json_input.cc
+++ tensorflow_io/json/kernels/json_input.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <iostream>
+#include <fstream>
 #include "kernels/dataset_ops.h"
 #include "tensorflow/core/lib/io/buffered_inputstream.h"
 #include <jsoncpp/json/json.h>
@@ -26,7 +28,86 @@ class JSONInput: public FileInput<io:BufferedInputStream> {
     if (state.get() == nullptr) {
       state.reset(new io::BufferedInputStream(s, 4096));
     }
-    return state.get()->ReadRecord(ctx, record_to_read, record_read, out_tensors);
+
+    // Read the json file
+    ifstream ifs(filename());
+    Json::Reader reader;
+    Json::Value records;
+
+    //Parse the json file to the json object
+    reader.parse(ifs, records);
+
+    while ((*record_read) < record_to_read && (*record_read) < records.size()) {
+      const Json::Value& record = records[*record_read];
+      if(*record_read == 0){
+        out_tensors->clear();
+        //allocate enough space for Tensor
+        for (size_t i = 0; i < columns().size(); i++){
+          const string& column = columns()[i];
+          const Json::Value& val = record[column];
+          DataType dtype;
+          switch (val.type()){
+          case Json::intValue:
+            dtype = DT_INT64;
+            break;
+          case Json::uintValue:
+            dtype = DT_UINT64;
+            break;
+          case Json::realValue:
+            dtype = DT_DOUBLE;
+            break;
+          case Json::stringValue:
+            dtype = DT_STRING;
+            break;
+          case Json::booleanValue:
+            dtype = DT_BOOL;
+            break;
+          //Currently Json::nullValue, Json::arrayValue, and Json::objectValue are not supported.
+          default:
+            return errors::InvalidArgument("Unsupported data type: ", val.type());
+          }
+          Tensor tensor(ctx->allocator({}), dtype, {record_to_read});
+          out_tensor->emplace_back(std::move(tensor));
+        }
+      }
+      for (size_t i = 0; i < columns().size(); i++) {
+        const string& column = columns()[i];
+        const Json::Value& val = record[column];
+        switch (val.type()){
+        case Json::intValue:
+          ((*out_tensors)[i]).flat<int64>()(*record_read) = val.asInt64();
+          break;
+        case Json::uintValue:
+          ((*out_tensors)[i]).flat<uint64>()(*record_read) = val.asUInt64();
+          break;
+        case Json::realValue:
+          ((*out_tensors)[i]).flat<double>()(*record_read) = val.asDouble();
+          break;
+        case Json::stringValue:
+          ((*out_tensors)[i]).flat<string>()(*record_read) = val.asString();
+          break;
+        case Json::booleanValue:
+          ((*out_tensors)[i]).flat<bool>()(*record_read) = val.asBool();
+          break;
+        //Currently Json::nullValue, Json::arrayValue, and Json::objectValue are not supported.
+        default:
+          return errors::InvalidArgument("Unsupported data type: ", val.type());
+        }
+      }
+      (*record_read)++;
+    }
+
+    //Slice if needed
+    if (*record_read < record_to_read) {
+      if (*record_read == 0) {
+        out_tensors->clear();
+      }
+      for (size_t i = 0; i < out_tensors->size(); i++) {
+        Tensor tensor = (*out_tensors)[i].Slice(0, *record_read);
+        (*out_tensors)[i] = std::move(tensor);
+      }
+    }
+    return Status::OK()
   }
   Status FromStream(io::InputStreamInterface* s) override {
     return Status::OK();
@@ -37,6 +118,13 @@ class JSONInput: public FileInput<io:BufferedInputStream> {
     return true;
   }
 };
+
+REGISTER_UNARY_VARIANT_DECODE_FUNCTION(JSONInput, "tensorflow::data::JSONInput");
+
+REGISTER_KERNEL_BUILDER(Name("JSONInput").Device(DEVICE_CPU),
+                        FileInputOp<JSONInput>);
+REGISTER_KERNEL_BUILDER(Name("JSONDataset").Device(DEVICE_CPU),
+                        FileInputDatasetOp<JSONInput, io::BufferedInputStream>);
 
 }  // namespace data
 }  // namespace tensorflow
