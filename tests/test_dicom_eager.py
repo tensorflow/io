d508484ef1310adcf504adbc8613f1486bc43809
Marcelo Lerendegui <marcelo@lerendegui.com>
2019-07-18 22:22:12 -0400

    Dicom initial commit (#359)
    * Dicom initial commit
    
    TODO: add tests for decode_dicom_image and decode_dicom_data
    
    Thanks @yongtang for guidance on merging into tensorflow-io
    
    * Fixed from __future__ import not at the beginning of the file
    Added fix to a few macOS issues dcmtk.BUILD. (by @yongtang)
    
    * Fixed coding style nonconformities referenced by @feihugis

---
+++ tests/test_dicom_eager.py
@@ -0,0 +1,37 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# ==============================================================================
+"""Tests for DICOM."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow_io.dicom as dicom_io  # pylint: disable=wrong-import-position
+
+if not (hasattr(tf, "version") and tf.version.VERSION.startswith("2.")):
+    tf.compat.v1.enable_eager_execution()
+
+
+def test_dicom_input():
+    """test_dicom_input
+    """
+    _ = dicom_io.decode_dicom_data
+    _ = dicom_io.decode_dicom_image
+    _ = dicom_io.tags
+
+
+if __name__ == "__main__":
+    test.main()
