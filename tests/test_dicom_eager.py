a4946c8428951ebc62e14b788d5fe7e09d13ed5e
Marcelo Lerendegui <marcelo@lerendegui.com>
2019-07-21 17:48:12 -0400

    Fix Lint (3 spaces)

--- tests/test_dicom_eager.py
+++ tests/test_dicom_eager.py
@@ -23,7 +23,7 @@ import pytest
 
 import tensorflow as tf
 if not (hasattr(tf, "version") and tf.version.VERSION.startswith("2.")):
-   tf.compat.v1.enable_eager_execution()
+  tf.compat.v1.enable_eager_execution()
 import tensorflow_io.dicom as dicom_io  # pylint: disable=wrong-import-position
 
 # The DICOM sample files must be downloaded befor running the tests
