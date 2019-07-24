8116c07b5adc92ff18785ace01f5c7bc6f08c333
Damien Pontifex <damien.pontifex@gmail.com>
2019-07-24 17:50:09 +1000

    travis yml refactor repeated code into script

--- .travis/bazel.build.sh
+++ .travis/bazel.build.sh
@@ -1,20 +1,16 @@
 set -e
 
-if [[ $(uname) == "Darwin" ]]; then
-# The -DGRPC_BAZEL_BUILD is needed because gRPC does not compile on macOS unless
-# it is set.
-  bazel build \
-    --copt=-DGRPC_BAZEL_BUILD \
-    --noshow_progress \
-    --noshow_loading_progress \
-    --verbose_failures \
-    --test_output=errors \
-    -- //tensorflow_io/...
-else
-  bazel build \
-    --noshow_progress \
-    --noshow_loading_progress \
-    --verbose_failures \
-    --test_output=errors \
-    -- //tensorflow_io/...
+args=()
+if [[ $(uname) == "Darwin" ]]; then 
+  # The -DGRPC_BAZEL_BUILD is needed because gRPC does not compile on macOS unless
+  # it is set.
+  args+=(--copt=-DGRPC_BAZEL_BUILD)
 fi
+
+bazel build \
+  "${args[@]}" \
+  --noshow_progress \
+  --noshow_loading_progress \
+  --verbose_failures \
+  --test_output=errors \
+  //tensorflow_io/...
