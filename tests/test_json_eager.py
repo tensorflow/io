975f4dd6b3ce1b8a0d4edbb10d06a7831420c029
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2019-07-26 07:52:02 +0200

    [GSoC-Phase2] Add JSON Support (#337)
    * Add json_ops
    
    * Add boilerplate code for json parser in cpp
    
    * use BufferInputStream instead of JSONInputStream
    
    * Add implementation of ReadRecord on the top of jsoncpp
    
    * Add JSONInputStream.
    
    * Remove the columns from JSONInputStream.
    
    * Fix a typo
    
    * Add and update the bazel BUILD files.
    
    * JSON cpp implementation can be compiled successfully.
    
    * Remove unused Python implementation of JSONDataset.
    
    * Fix lints.
    
    * Fix lints.
    
    * Fix lints.
    
    * Fix bugs and improve the tests.
    
    * Fix lints
    
    * Add a test to use JSONDataset with Keras.
    
    * Fix a lint

--- tests/test_json_eager.py
+++ tests/test_json_eager.py
@@ -30,12 +30,8 @@ import tensorflow_io.json as json_io  # pylint: disable=wrong-import-position
 def test_json_dataset():
   """Test case for JSON Dataset.
   """
-  json_filename = os.path.join(
-      os.path.dirname(os.path.abspath(__file__)),
-      "test_json",
-      "json_test.npz")
-  with np.load(json_filename) as f:
-    (x_test, y_test) = f["x_test"], f["y_test"]
+  x_test = [[1.1, 2], [2.1, 3]]
+  y_test = [[2.2, 3], [1.2, 3]]
   feature_filename = os.path.join(
       os.path.dirname(os.path.abspath(__file__)),
       "test_json",
@@ -45,31 +41,45 @@ def test_json_dataset():
       "test_json",
       "label.json")
 
-  feature_dataset = json_io.JSONDataset(feature_filename)
-  label_dataset = json_io.JSONDataset(label_filename)
+  feature_list = ["floatfeature", "integerfeature"]
+  label_list = ["floatlabel", "integerlabel"]
+  feature_dataset = json_io.JSONDataset(
+      feature_filename,
+      feature_list,
+      [tf.float64, tf.int64])
+  label_dataset = json_io.JSONDataset(
+      label_filename,
+      label_list,
+      [tf.float64, tf.int64])
 
   i = 0
-  for j_x in feature_dataset:
+  for record in feature_dataset:
     v_x = x_test[i]
-    assert np.alltrue(v_x == j_x.numpy())
+    for index, val in enumerate(record):
+      assert v_x[index] == val.numpy()
     i += 1
   assert i == len(y_test)
 
   ## Test of the reverse order of the columns
   feature_list = ["integerfeature", "floatfeature"]
-  feature_dataset = json_io.JSONDataset(feature_filename, feature_list)
+  feature_dataset = json_io.JSONDataset(
+      feature_filename,
+      feature_list,
+      [tf.int64, tf.float64])
 
   i = 0
-  for j_x in feature_dataset:
+  for record in feature_dataset:
     v_x = np.flip(x_test[i])
-    assert np.alltrue(v_x == j_x.numpy())
+    for index, val in enumerate(record):
+      assert v_x[index] == val.numpy()
     i += 1
   assert i == len(y_test)
 
   i = 0
-  for j_y in label_dataset:
+  for record in label_dataset:
     v_y = y_test[i]
-    assert np.alltrue(v_y == j_y.numpy())
+    for index, val in enumerate(record):
+      assert v_y[index] == val.numpy()
     i += 1
   assert i == len(y_test)
 
@@ -82,11 +92,58 @@ def test_json_dataset():
   for (j_x, j_y) in dataset:
     v_x = np.flip(x_test[i])
     v_y = y_test[i]
-    assert np.alltrue(v_y == j_y.numpy())
-    assert np.alltrue(v_x == j_x.numpy())
+    for index, x in enumerate(j_x):
+      assert v_x[index] == x.numpy()
+    for index, y in enumerate(j_y):
+      assert v_y[index] == y.numpy()
     i += 1
   assert i == len(y_test)
 
+def test_json_keras():
+  """Test case for JSONDataset with keras."""
+  feature_filename = os.path.join(
+      os.path.dirname(os.path.abspath(__file__)),
+      "test_json",
+      "iris.json")
+  label_filename = os.path.join(
+      os.path.dirname(os.path.abspath(__file__)),
+      "test_json",
+      "species.json")
+
+  feature_list = ['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth']
+  label_list = ["species"]
+  feature_types = [tf.float64, tf.float64, tf.float64, tf.float64]
+  label_types = [tf.int64]
+  feature_dataset = json_io.JSONDataset(
+      feature_filename,
+      feature_list,
+      feature_types,
+      batch=32)
+  label_dataset = json_io.JSONDataset(
+      label_filename,
+      label_list,
+      label_types,
+      batch=32)
+  dataset = tf.data.Dataset.zip((
+      feature_dataset,
+      label_dataset
+  ))
+  def pack_features_vector(features, labels):
+    """Pack the features into a single array."""
+    features = tf.stack(list(features), axis=1)
+    return features, labels
+  dataset = dataset.map(pack_features_vector)
+
+  model = tf.keras.Sequential([
+      tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
+      tf.keras.layers.Dense(10, activation=tf.nn.relu),
+      tf.keras.layers.Dense(3)
+  ])
+
+  model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+  model.fit(dataset, epochs=5)
 
 if __name__ == "__main__":
   test.main()
