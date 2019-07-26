5bfe331a7f93c6816ba68989031279389ad96484
Florian Raudies <florian.raudies@gmail.com>
2019-07-26 15:25:39 -0700

    Remove old implementations of avro_record_dataset and parse_avro_record - Why? These implementation are obsolete and we would like to phase them out - What? Remove the implementations and the lib avroc setup from tensorflow-io
    RB=1741775
    G=tensorflow-reviewers,tony-reviewers
    R=xhzhang,cgu,khu
    A=khu

--- tensorflow_io/avro/ops/avro_ops.cc
+++ tensorflow_io/avro/ops/avro_ops.cc
@@ -12,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <avro.h> // TODO(fraudies): Remove me when old ops are removed
-
 #include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
@@ -22,7 +20,6 @@ limitations under the License.
 namespace tensorflow {
 
 using ::tensorflow::shape_inference::ShapeHandle;
-
 
 REGISTER_OP("AvroDataset")
     .Input("filenames: string")
@@ -81,128 +78,5 @@ REGISTER_OP("AvroDataset")
 
       return shape_inference::ScalarShape(c);
     });
-
-
-// Register the avro record dataset operator
-REGISTER_OP("AvroRecordDataset")
-    .Input("filenames: string")
-    .Input("schema: string")
-    .Input("buffer_size: int64")
-    .Output("handle: variant")
-    .SetIsStateful()
-    .SetShapeFn(shape_inference::ScalarShape)
-    .Doc(R"doc(
-Creates a dataset that emits the avro records from one or more files.
-filenames: A scalar or vector containing the name(s) of the file(s) to be
-  read.
-schema: A string used that is used for schema resolution.
-)doc");
-
-// Register the parse function when building the shared library
-// For the op I used as boiler plate: tensorflow/core/ops/parsing_ops.cc and
-// there 'ParseExample'
-// For the op kernel I used as boiler plate:
-// tensorflow/core/kernels/example_parsing_ops.cc and there 'ExampleParserOp'
-// For the compute method I used as boiler plate:
-// tensorflow/core/util/example_proto_fast_parsing.cc and there the
-//   method 'FastParseExample'
-
-REGISTER_OP("ParseAvroRecord")
-    .Input("serialized: string")
-    .Input("sparse_keys: Nsparse * string")
-    .Input("dense_keys: Ndense * string")
-    .Input("dense_defaults: Tdense")
-    .Output("sparse_indices: Nsparse * int64")
-    .Output("sparse_values: sparse_types")
-    .Output("sparse_shapes: Nsparse * int64")
-    .Output("dense_values: Tdense")
-    .Attr("Nsparse: int >= 0")  // Inferred from sparse_keys
-    .Attr("Ndense: int >= 0")   // Inferred from dense_keys
-    .Attr("sparse_types: list({float,double,int64,int32,string,bool}) >= 0")
-    .Attr("Tdense: list({float,double,int64,int32,string,bool}) >= 0")
-    .Attr("dense_shapes: list(shape) >= 0")
-    .Attr("schema: string")
-    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-
-       data::ParseAvroAttrs attrs;
-       TF_RETURN_IF_ERROR(attrs.Init(c));
-
-       // Get the batch size and load it into input
-       ShapeHandle input;
-       TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 1, &input));
-
-       // Get the schema, parse it, and log it
-       string schema;
-       TF_RETURN_IF_ERROR(c->GetAttr("schema", &schema));
-
-       std::unique_ptr<avro_schema_t, std::function<void(avro_schema_t*)>>
-           p_reader_schema(new avro_schema_t, [](avro_schema_t* ptr) {
-             avro_schema_decref(*ptr);
-           });
-       if (avro_schema_from_json_length(schema.c_str(), schema.length(),
-                                        &(*p_reader_schema)) != 0) {
-         return errors::InvalidArgument("The provided json schema is invalid. ",
-                                        avro_strerror());
-       }
-       LOG(INFO) << "Avro parser with schema\n" << schema;
-
-       int output_idx = 0;
-
-       // Output sparse_indices, sparse_values, sparse_shapes
-       for (int i_sparse = 0; i_sparse < attrs.num_sparse; ++i_sparse) {
-         c->set_output(output_idx++, c->Matrix(c->UnknownDim(), 2));
-       }
-       for (int i_sparse = 0; i_sparse < attrs.num_sparse; ++i_sparse) {
-         c->set_output(output_idx++, c->Vector(c->UnknownDim()));
-       }
-       for (int i_sparse = 0; i_sparse < attrs.num_sparse; ++i_sparse) {
-         c->set_output(output_idx++, c->Vector(2));
-       }
-
-       // Output dense_values
-       for (int i_dense = 0; i_dense < attrs.num_dense; ++i_dense) {
-         ShapeHandle dense;
-         TF_RETURN_IF_ERROR(c->MakeShapeFromPartialTensorShape(
-             attrs.dense_infos[i_dense].shape, &dense));
-         TF_RETURN_IF_ERROR(c->Concatenate(input, dense, &dense));
-         c->set_output(output_idx++, dense);
-       }
-
-       return Status::OK();
-     })
-    .Doc(R"doc(
-      Parses a serialized avro record that follows the supplied schema into typed tensors.
-      serialized: A vector containing a batch of binary serialized avro records.
-      dense_keys: A list of Ndense string Tensors.
-        The keys expected are associated with dense values.
-      dense_defaults: A list of Ndense Tensors (some may be empty).
-        These defaults can either be fully defined for all values in the tensor or they
-        can be defined as padding element that is used whenever the original input data
-        misses values to fill out the full tensor.
-      dense_shapes: A list of Ndense shapes; the shapes of the dense tensors.
-        The number of elements corresponding to dense_key[j]
-        must always equal dense_shapes[j].NumEntries().
-        If dense_shapes[j] == (D0, D1, ..., DN) then the shape of output
-        Tensor dense_values[j] will be (|serialized|, D0, D1, ..., DN):
-        The dense outputs are just the inputs row-stacked by batch.
-        This works for dense_shapes[j] = (-1, D1, ..., DN).  In this case
-        the shape of the output Tensor dense_values[j] will be
-        (|serialized|, M, D1, .., DN), where M is the maximum number of blocks
-        of elements of length D1 * .... * DN, across all minibatch entries
-        in the input.  Any minibatch entry with less than M blocks of elements of
-        length D1 * ... * DN will be padded with the corresponding default_value
-        scalar element along the second dimension.
-      dense_types: A list of Ndense types; the type of data in each Feature given in dense_keys.
-      sparse_keys: A list of Ndense string Tensors (scalars).
-        The keys expected are associated with sparse values.
-      sparse_types: A list of Nsparse types; the data types of data in each Feature
-        given in sparse_keys.
-      schema: A string that describes the avro schema of the underlying serialized avro string.
-        Currently the parse function supports the primitive types DT_STRING, DT_DOUBLE, DT_FLOAT,
-        DT_INT64, DT_INT32, and DT_BOOL.
-        The supported avro version depends on the compiled library avro library linked against
-        TensorFlow during build. More instructions on avro:
-        https://avro.apache.org/docs/1.8.1/spec.html
-    )doc");
 
 }  // namespace tensorflow
--- tensorflow_io/avro/utils/parse_avro_attrs.cc
+++ tensorflow_io/avro/utils/parse_avro_attrs.cc
@@ -12,43 +12,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include <string>
-#include <vector>
 
-#include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow_io/avro/utils/parse_avro_attrs.h"
 #include "tensorflow/core/framework/types.h"
 #include "tensorflow/core/platform/types.h"
-#include "tensorflow/core/framework/partial_tensor_shape.h"
 #include "tensorflow/core/lib/core/errors.h"
-#include "tensorflow_io/avro/utils/parse_avro_attrs.h"
 
 namespace tensorflow {
 namespace data {
-
-Status CheckDenseShapeToBeDefined(
-    const std::vector<PartialTensorShape>& dense_shapes) {
-  for (int i = 0; i < dense_shapes.size(); ++i) {
-    const PartialTensorShape& dense_shape = dense_shapes[i];
-    bool shape_ok = true;
-    if (dense_shape.dims() == -1) {
-      shape_ok = false;
-    } else {
-      for (int d = 1; d < dense_shape.dims() && shape_ok; ++d) {
-        if (dense_shape.dim_size(d) == -1) {
-          shape_ok = false;
-        }
-      }
-    }
-    if (!shape_ok) {
-      return errors::InvalidArgument(
-          "dense_shapes[", i,
-          "] has unknown rank or unknown inner dimensions: ",
-          dense_shape.DebugString());
-    }
-  }
-  return Status::OK();
-}
 
 // As boiler plate I used tensorflow/core/util/example_proto_helper.cc and
 // therein "ParseSingleExampleAttrs" and
@@ -65,29 +36,6 @@ Status CheckValidType(const DataType& dtype) {
       return errors::InvalidArgument("Received input dtype: ",
                                      DataTypeString(dtype));
   }
-}
-
-// Finishes the initialization for the attributes, which essentially checks that
-// the attributes have the correct values.
-//
-// returns OK if all attributes are valid; otherwise false.
-Status ParseAvroAttrs::FinishInit() {
-  if (static_cast<size_t>(num_sparse) != sparse_types.size()) {
-    return errors::InvalidArgument("len(sparse_keys) != len(sparse_types)");
-  }
-  if (static_cast<size_t>(num_dense) != dense_infos.size()) {
-    return errors::InvalidArgument("len(dense_keys) != len(dense_infos)");
-  }
-  if (num_dense > std::numeric_limits<int32>::max()) {
-    return errors::InvalidArgument("num_dense_ too large");
-  }
-  for (const DenseInformation& dense_info : dense_infos) {
-    TF_RETURN_IF_ERROR(CheckValidType(dense_info.type));
-  }
-  for (const DataType& type : sparse_types) {
-    TF_RETURN_IF_ERROR(CheckValidType(type));
-  }
-  return Status::OK();
 }
 
 }  // namespace data
