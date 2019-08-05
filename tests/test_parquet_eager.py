1642da13c92777857af82880aab425d5dee16aed
Yong Tang <yong.tang.github@outlook.com>
2019-08-05 14:04:11 -0700

    Rework on ParquetDataset for easy access and better cache size in eager mode (#384)
    * Rework on ParquetDataset for easy access and better cache size in eager mode
    
    This fix is part of the effort to improve overall Dataset for
    easy access and better cache size in eager mode.
    See 382 and 366 for related discussions.
    
    In order to be able to read file either in filename or in mmeory, this PR
    adds an SizedRandomAccessFile which allows to provide an optional memory buffer
    as file content. This could be useful in process compression or archives
    where we could just read the uncompressed file content into memory.
    
    The preivous limitation in Dataset was that Dataset was a iterable so sequence
    length is unknown until graph runtime. In this PR, we provide an helper function
    to read the specs of parquet file and lenth is know.
    
    This also could open other avenues such as map parquet file with __getitem__ and __len__.
    Further, parquet file could be read into a Tensor and processed easily (such as pandas like API).
    
    The read_parquet_specs could be similarly applied to HDF5 which is more important:
    HDF5 could have dataset with different sizes.
    
    Summary:
    1) Two basic C++ kernel ops are implemnted: read_parquet_specs and read_parquet
    2) One ParquetDataset that is python implementation only (no C++ anymore)
    3) ParquetDataset support eager and graph mode, in graph mode, dtype and shape
       are provided by user explicitly. In eager mode, only column name is needed.
    4) read_parquet works in eager and graph mode, can read records either in full, or in slices
    5) read_parquet_specs works in eager mode only (limitation).
    
    For cache batch vs. batch in tf.keras
    1) Added a hidden `capacity` to adjust the cache batch size
    2) batch to be passed in tf.keras is unrelated to `capacity`, but we could use `rebatch`
       to change at the end of the pipeline.
    3) `capacity` could be padded to allow `rebatch` to only cut a slice over one chunk.
       If not padded to `batch_size` in tf.keras, then `rebatch` likely will copy over boundary.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Fix build failures
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Rename read_parquet_columns => list_parquet_columns
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Remove batch args, and add test in graph mode
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tests/test_parquet_eager.py
+++ tests/test_parquet_eager.py
@@ -12,18 +12,18 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 # ==============================================================================
-"""Tests for ParquetDataset."""
+"""Tests for read_parquet and ParquetDataset."""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
-import pytest
 import numpy as np
 
 import tensorflow as tf
-tf.compat.v1.disable_eager_execution()
+if not (hasattr(tf, "version") and tf.version.VERSION.startswith("2.")):
+  tf.compat.v1.enable_eager_execution()
 import tensorflow_io.parquet as parquet_io # pylint: disable=wrong-import-position
 
 # Note: The sample file is generated from:
@@ -38,52 +38,58 @@ import tensorflow_io.parquet as parquet_io # pylint: disable=wrong-import-positi
 # Column 4 (float): Equal to row_index * 1.1.
 # Column 5 (double): Equal to row_index * 1.1111111.
 def test_parquet():
-  """Test case for ParquetDataset."""
+  """Test case for read_parquet.
+
+  """
   filename = os.path.join(
       os.path.dirname(os.path.abspath(__file__)),
       "test_parquet",
       "parquet_cpp_example.parquet")
   filename = "file://" + filename
 
+  specs = parquet_io.list_parquet_columns(filename)
   columns = [
       'boolean_field',
       'int32_field',
       'int64_field',
       'float_field',
       'double_field']
-  dtypes = [
-      tf.bool,
-      tf.int32,
-      tf.int64,
-      tf.float32,
-      tf.double]
+  p0 = parquet_io.read_parquet(filename, specs['boolean_field'])
+  p1 = parquet_io.read_parquet(filename, specs['int32_field'])
+  p2 = parquet_io.read_parquet(filename, specs['int64_field'])
+  p4 = parquet_io.read_parquet(filename, specs['float_field'])
+  p5 = parquet_io.read_parquet(filename, specs['double_field'])
+
+  for i in range(500): # 500 rows.
+    v0 = ((i % 2) == 0)
+    v1 = i
+    v2 = i * 1000 * 1000 * 1000 * 1000
+    v4 = 1.1 * i
+    v5 = 1.1111111 * i
+    assert v0 == p0[i].numpy()
+    assert v1 == p1[i].numpy()
+    assert v2 == p2[i].numpy()
+    assert np.isclose(v4, p4[i].numpy())
+    assert np.isclose(v5, p5[i].numpy())
 
   dataset = tf.compat.v2.data.Dataset.zip(
-      tuple([parquet_io.ParquetDataset(
-          filename, column, dtype=dtype,
-          start=0, stop=500) for (
-              column, dtype) in zip(columns, dtypes)])).apply(
-                  tf.data.experimental.unbatch())
-
-  iterator = tf.compat.v1.data.make_initializable_iterator(dataset)
-  init_op = iterator.initializer
-  get_next = iterator.get_next()
-  with tf.compat.v1.Session() as sess:
-    sess.run(init_op)
-    for i in range(500):
-      v0 = ((i % 2) == 0)
-      v1 = i
-      v2 = i * 1000 * 1000 * 1000 * 1000
-      v4 = 1.1 * i
-      v5 = 1.1111111 * i
-      p0, p1, p2, p4, p5 = sess.run(get_next)
-      assert v0 == p0
-      assert v1 == p1
-      assert v2 == p2
-      assert np.isclose(v4, p4)
-      assert np.isclose(v5, p5)
-    with pytest.raises(tf.errors.OutOfRangeError):
-      sess.run(get_next)
+      tuple(
+          [parquet_io.ParquetDataset(filename, column) for column in columns])
+  ).apply(tf.data.experimental.unbatch())
+  i = 0
+  for p in dataset:
+    v0 = ((i % 2) == 0)
+    v1 = i
+    v2 = i * 1000 * 1000 * 1000 * 1000
+    v4 = 1.1 * i
+    v5 = 1.1111111 * i
+    p0, p1, p2, p4, p5 = p
+    assert v0 == p0.numpy()
+    assert v1 == p1.numpy()
+    assert v2 == p2.numpy()
+    assert np.isclose(v4, p4.numpy())
+    assert np.isclose(v5, p5.numpy())
+    i += 1
 
 if __name__ == "__main__":
   test.main()
