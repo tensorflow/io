c3f1b7dc536df435b5dc4eb5bfcfaf5a0df16c61
Yong Tang <yong.tang.github@outlook.com>
2019-08-08 22:34:28 -0700

    Deprecate SequenceFile Dataset (#415)
    SequenceFile Dataset was never in good use,
    and the trend of industry aggressively switching from
    self-managed Hadoop system to cloud-vendor provided storage (S3/GCS/etc)
    makes this Dataset format less and less relevant.
    This PR deprecates SequenceFile Dataset
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/hadoop/__init__.py
+++ tensorflow_io/hadoop/__init__.py
@@ -21,9 +21,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import warnings
+
 from tensorflow_io.hadoop.python.ops.hadoop_dataset_ops import SequenceFileDataset
 
 from tensorflow.python.util.all_util import remove_undocumented
+
+warnings.warn(
+    "SequenceFileDataset is deprecated and will be removed in the next release",
+    DeprecationWarning)
 
 _allowed_symbols = [
     "SequenceFileDataset",
