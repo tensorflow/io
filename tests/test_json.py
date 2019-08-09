ada18c0c8ca761d22670124a17e73eb43f0d4503
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2019-08-09 06:43:15 +0200

    Add read_json and list_json_columns for JSON support. (#412)

--- tests/test_json.py
+++ tests/test_json.py
@@ -19,49 +19,43 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import numpy as np
+import pytest
+
 import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
-
-from tensorflow import dtypes  # pylint: disable=wrong-import-position
-from tensorflow import errors  # pylint: disable=wrong-import-position
-from tensorflow import test    # pylint: disable=wrong-import-position
-from tensorflow.compat.v1 import data # pylint: disable=wrong-import-position
-
 import tensorflow_io.json as json_io # pylint: disable=wrong-import-position
 
 
+def test_json_dataset():
+  """Test case for JSONDataset."""
+  filename = os.path.join(
+      os.path.dirname(os.path.abspath(__file__)),
+      "test_json",
+      "feature.json")
+  filename = "file://" + filename
 
-class JSONDatasetTest(test.TestCase):
-  """JSONDatasetTest"""
+  num_repeats = 2
+  dataset = tf.compat.v2.data.Dataset.zip(
+      (
+          json_io.JSONDataset(filename, "floatfeature", dtype=tf.float64),
+          json_io.JSONDataset(filename, "integerfeature", dtype=tf.int64)
+      )).repeat(num_repeats).apply(tf.data.experimental.unbatch())
 
-  def test_json_dataset(self):
-    """Test case for JSONDataset."""
-    filename = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "test_json",
-        "feature.json")
-    filename = "file://" + filename
+  iterator = tf.compat.v1.data.make_initializable_iterator(dataset)
+  init_op = iterator.initializer
+  get_next = iterator.get_next()
 
-    columns = ['floatfeature', 'integerfeature']
-    output_types = (dtypes.float64, dtypes.int64)
-    num_repeats = 2
-
-    dataset = json_io.JSONDataset(
-        filename, columns=columns, dtypes=output_types).repeat(num_repeats)
-    iterator = data.make_initializable_iterator(dataset)
-    init_op = iterator.initializer
-    get_next = iterator.get_next()
-
-    test_json = [(1.1, 2), (2.1, 3)]
-    with self.test_session() as sess:
-      sess.run(init_op)
-      for _ in range(num_repeats):
-        for i in range(2):
-          (floatf, intf) = test_json[i]
-          vv = sess.run(get_next)
-          self.assertAllClose((floatf, intf), vv)
-      with self.assertRaises(errors.OutOfRangeError):
-        sess.run(get_next)
+  test_json = [(1.1, 2), (2.1, 3)]
+  with tf.compat.v1.Session() as sess:
+    sess.run(init_op)
+    for _ in range(num_repeats):
+      for i in range(2):
+        (floatf, intf) = test_json[i]
+        vv = sess.run(get_next)
+        np.allclose((floatf, intf), vv)
+    with pytest.raises(tf.errors.OutOfRangeError):
+      sess.run(get_next)
 
 if __name__ == "__main__":
   test.main()
