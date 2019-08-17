5d9bbfd6dd69ae03225be31ab646b8ef2c7410ab
Yong Tang <yong.tang.github@outlook.com>
2019-08-17 14:49:11 +0000

    Update wheel test
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .travis/wheel.configure.sh
+++ .travis/wheel.configure.sh
@@ -2,27 +2,7 @@ set -e
 
 python --version
 python -m pip --version
-if [[ $(uname) == "Darwin" ]]; then
-  if [[ "$1" == "--"* ]]; then
-    shift
-    shift
-  fi
-  entry=$1
-  if [[ $entry == *"3"* ]]; then
-    # If on macOS and building python3 packages
-    minor=$(echo $entry | cut -d. -f2)
-    patch=$(echo $entry | cut -d. -f3)
-    # Specify version number for local use
-    pyenv install --skip-existing "3.${minor}.${patch}"
-    pyenv local "3.${minor}.${patch}"
-    # Drop patch number from executable command
-    entry=$(pyenv which python)
-    $entry -m pip install -q wheel==0.31.1
-  fi
-
-  python -m pip install -q delocate
-  delocate-wheel --version
-else
+if [[ $(uname) == "Linux" ]]; then
   apt-get -y -qq update
   apt-get -y -qq install software-properties-common apt-transport-https
   add-apt-repository -y ppa:deadsnakes/ppa
