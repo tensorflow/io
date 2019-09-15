3ba3c5fb807b8deccaaec033cf1027e3d3fc1dc1
Suyash Kumar <suyashkumar2003@gmail.com>
2019-09-14 23:45:34 -0700

    Initial conversion of sequences to onehot RaggedTensors using tf.function

--- tensorflow_io/genome/python/ops/genome_ops.py
+++ tensorflow_io/genome/python/ops/genome_ops.py
@@ -17,6 +17,43 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
 from tensorflow_io.core.python.ops import core_ops
 
 read_fastq = core_ops.read_fastq
+
+A_ONEHOT = tf.constant([1, 0, 0, 0])
+C_ONEHOT = tf.constant([0, 1, 0, 0])
+G_ONEHOT = tf.constant([0, 0, 1, 0])
+T_ONEHOT = tf.constant([0, 0, 0, 1])
+ERROR = tf.constant([1, 1, 1, 1])
+
+
+@tf.function
+def _nucleotide_to_onehot(nucleotide):
+    if tf.math.equal(nucleotide, tf.constant(b'A')):
+        return A_ONEHOT
+    elif tf.math.equal(nucleotide, tf.constant(b'C')):
+        return C_ONEHOT
+    elif tf.math.equal(nucleotide, tf.constant(b'G')):
+        return G_ONEHOT
+    elif tf.math.equal(nucleotide, tf.constant(b'T')):
+        return T_ONEHOT
+    else:
+        # TODO(suyashkumar): how best to raise error from within tf.function?
+        return ERROR
+
+
+@tf.function
+def sequences_to_onehot(sequences):
+    sequences_onehot = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+    splits = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+    i = 0
+    splits = splits.write(splits.size(), i)
+    for sequence in sequences:
+        split_nucleotides = tf.strings.bytes_split(sequence)
+        for nucleotide in split_nucleotides:
+            sequences_onehot = sequences_onehot.write(sequences_onehot.size(), _nucleotide_to_onehot(nucleotide))
+            i = i + 1
+        splits = splits.write(splits.size(), i)
+    return tf.RaggedTensor.from_row_splits(values=sequences_onehot.stack(), row_splits=splits.stack())
