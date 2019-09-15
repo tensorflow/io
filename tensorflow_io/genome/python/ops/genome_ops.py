08e90620f0ee1027ea45ebe6928392e9277dbbce
Suyash Kumar <suyashkumar2003@gmail.com>
2019-09-15 02:21:38 -0700

    Docstrings, lint, cleanup.

--- tensorflow_io/genome/python/ops/genome_ops.py
+++ tensorflow_io/genome/python/ops/genome_ops.py
@@ -31,29 +31,51 @@ ERROR = tf.constant([1, 1, 1, 1])
 
 @tf.function
 def _nucleotide_to_onehot(nucleotide):
-    if tf.math.equal(nucleotide, tf.constant(b'A')):
-        return A_ONEHOT
-    elif tf.math.equal(nucleotide, tf.constant(b'C')):
-        return C_ONEHOT
-    elif tf.math.equal(nucleotide, tf.constant(b'G')):
-        return G_ONEHOT
-    elif tf.math.equal(nucleotide, tf.constant(b'T')):
-        return T_ONEHOT
-    else:
-        # TODO(suyashkumar): how best to raise error from within tf.function?
-        return ERROR
+  """Encodes a nucleotide using a one hot encoding."""
+  if tf.math.equal(nucleotide, tf.constant(b'A')):
+    return A_ONEHOT
+  elif tf.math.equal(nucleotide, tf.constant(b'C')):
+    return C_ONEHOT
+  elif tf.math.equal(nucleotide, tf.constant(b'G')):
+    return G_ONEHOT
+  elif tf.math.equal(nucleotide, tf.constant(b'T')):
+    return T_ONEHOT
+  else:
+    # TODO(suyashkumar): how best to raise error from within tf.function?
+    return ERROR
 
 
 @tf.function
 def sequences_to_onehot(sequences):
-    sequences_onehot = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
-    splits = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
-    i = 0
-    splits = splits.write(splits.size(), i)
-    for sequence in sequences:
-        split_nucleotides = tf.strings.bytes_split(sequence)
-        for nucleotide in split_nucleotides:
-            sequences_onehot = sequences_onehot.write(sequences_onehot.size(), _nucleotide_to_onehot(nucleotide))
-            i = i + 1
-        splits = splits.write(splits.size(), i)
-    return tf.RaggedTensor.from_row_splits(values=sequences_onehot.stack(), row_splits=splits.stack())
+  """Convert DNA sequences into a one hot nucleotide encoding.
+
+  Each nucleotide in each sequence is mapped as follows:
+  A -> [1, 0, 0, 0]
+  C -> [0, 1, 0, 0]
+  G -> [0 ,0 ,1, 0]
+  T -> [0, 0, 0, 1]
+
+  Args:
+    sequences: A tf.string tensor where each string represents a DNA sequence
+
+  Returns:
+    tf.RaggedTensor: The output sequences with nucleotides one hot endcoded.
+  """
+  all_onehot_nucleotides = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+  sequence_splits = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+
+  global_nucleotide_idx = 0
+  sequence_splits = sequence_splits.write(sequence_splits.size(), global_nucleotide_idx)
+
+  for sequence in sequences:
+    for nucleotide in tf.strings.bytes_split(sequence):
+      all_onehot_nucleotides = all_onehot_nucleotides.write(
+        global_nucleotide_idx,
+        _nucleotide_to_onehot(nucleotide)
+      )
+      global_nucleotide_idx += 1
+    sequence_splits = sequence_splits.write(sequence_splits.size(), global_nucleotide_idx)
+  return tf.RaggedTensor.from_row_splits(
+    values=all_onehot_nucleotides.stack(),
+    row_splits=sequence_splits.stack()
+  )
