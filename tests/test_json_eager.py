1219050ace0262e38e8f29b8b8507519972a38e1
Yong Tang <yong.tang.github@outlook.com>
2019-09-21 15:27:14 +0000

    Fix 1.15.0 compatibility issue
    This PR fixes several 1.15.0 compatibility issue. It works for
    both TF 1.15.0 and 2.0.0.
    
    One this PR is merged to master, we will rebase R0.8 branch.
    
    Note the issue was casued by bumping R0.8 from 1.15.0rc0 to 1.15.0rc1
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tests/test_json_eager.py
+++ tests/test_json_eager.py
@@ -76,9 +76,9 @@ def test_io_tensor_json_recods_mode():
     v_x = x_test[i]
     v_y = y_test[i]
     for index, x in enumerate(j_x):
-      assert v_x[index] == x
+      assert v_x[index] == x.numpy()
     for index, y in enumerate(j_y):
-      assert v_y[index] == y
+      assert v_y[index] == y.numpy()
     i += 1
   assert i == len(y_test)
 
