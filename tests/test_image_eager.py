7aa23c055441e61e67b94b226fc09b2f900a88ea
Yong Tang <yong.tang.github@outlook.com>
2019-09-25 10:42:08 -0700

    Add decode_tiff and decode_tiff_info (#504)
    This PR adds native kernel ops of decode_tiff and decode_tiff_info
    to replace the previous implementation of TIFFDataset.
    
    The reasons to prefer native kernel ops for images are
    1) Images are normally one item per file. Iteration does not make a lot of sense
    2) Images fit into memory which means there is no need to "partially" load a slice like other data
       formats that may store huge data (e.g., Parquet).
    
    Note tiff may have multiple frames with different shapes. This PR
    introduce the `index` input to allow reading one frame at a time.
    
    This PR also wraps TIFF into a TIFFIOTensor to allow easy access.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

---
+++ tests/test_image_eager.py
@@ -0,0 +1,62 @@
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+# ==============================================================================
+"""Tests for ImageIOTensor."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+
+import tensorflow as tf
+if not (hasattr(tf, "version") and tf.version.VERSION.startswith("2.")):
+  tf.compat.v1.enable_eager_execution()
+import tensorflow_io as tfio # pylint: disable=wrong-import-position
+
+
+def test_tiff_io_tensor():
+  """Test case for TIFFImageIOTensor"""
+  width = 560
+  height = 320
+  channels = 4
+
+  images = []
+  for filename in [
+      "small-00.png",
+      "small-01.png",
+      "small-02.png",
+      "small-03.png",
+      "small-04.png"]:
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                     "test_image",
+                     filename), 'rb') as f:
+      png_contents = f.read()
+    image_v = tf.image.decode_png(png_contents, channels=channels)
+    assert image_v.shape == [height, width, channels]
+    images.append(image_v)
+
+  filename = os.path.join(
+      os.path.dirname(os.path.abspath(__file__)), "test_image", "small.tiff")
+  filename = "file://" + filename
+
+  tiff = tfio.IOTensor.from_tiff(filename)
+  assert tiff.keys == list(range(5))
+  for i in tiff.keys:
+    assert np.all(images[i] == tiff(i).to_tensor())
+
+
+if __name__ == "__main__":
+  test.main()
