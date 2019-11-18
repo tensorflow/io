7d31ea1190cfd5bc9e8239b7829bd3d3f0f05767
Yong Tang <yong.tang.github@outlook.com>
2019-11-18 12:35:45 -0800

    Remove verbose output of lint with bazel
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .travis/lint.sh
+++ .travis/lint.sh
@@ -11,18 +11,18 @@ fi
 # Note: clang-format is not enabled yet
 if [[ "$#" -gt 0 && "$1" == "clang" ]]; then
   for f in $(find tensorflow_io/ -name '*.cc' -o -name '*.h'); do
-    diff -u <(cat $f) <(bazel run @llvm_toolchain//:bin/clang-format -- --style=google $f)
+    diff -u <(cat $f) <(bazel run --noshow_progress --noshow_loading_progress --verbose_failures --test_output=errors @llvm_toolchain//:bin/clang-format -- --style=google $f)
   done
 fi
 if [[ "$#" -gt 0 && "$1" == "clang-git-diff" ]]; then
   for f in $(git diff --name-status master -- 'tensorflow_io/*.cc' 'tensorflow_io/*.h' | awk '{print $2}'); do
-    diff -u <(cat $f) <(bazel run @llvm_toolchain//:bin/clang-format -- --style=google $f)
+    diff -u <(cat $f) <(bazel run --noshow_progress --noshow_loading_progress --verbose_failures --test_output=errors @llvm_toolchain//:bin/clang-format -- --style=google $f)
   done
 fi
 
 # Note: `bazel run @com_github_bazelbuild_buildtools//:buildifier` fixes the lint
 # while below checks the lint
-bazel run --run_under="cd $PWD && " @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=diff $(find . -type f \( -name WORKSPACE -or -name BUILD -or -name '*.BUILD' \))
+bazel run --noshow_progress --noshow_loading_progress --verbose_failures --test_output=errors --run_under="cd $PWD && " @com_github_bazelbuild_buildtools//buildifier:buildifier -- --mode=diff $(find . -type f \( -name WORKSPACE -or -name BUILD -or -name '*.BUILD' \))
 
 docker run -i -t --rm -v $PWD:/v -w /v --net=host python:2.7-slim bash -x -e .travis/lint.sh python
 
