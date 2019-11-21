0c9bd77563d13b5e38b7b7adf11728fcd432bc48
Ann Yan <anyan@linkedin.com>
2019-11-20 18:38:27 -0800

    [LINKEDIN-AVRO] Add specific reshape to fix rank of sparse tensor output from avrodataset
    RB=1888890
    G=tensorflow-reviewers,tony-reviewers
    R=xhzhang,fraudies
    A=xhzhang,fraudies

--- tensorflow_io/avro/python/tests/avro_dataset_test_base.py
+++ tensorflow_io/avro/python/tests/avro_dataset_test_base.py
@@ -22,6 +22,7 @@ from __future__ import print_function
 import logging
 import numpy as np
 import os
+import re
 import six
 import tempfile
 
@@ -47,6 +48,45 @@ class AvroDatasetTestBase(test_base.DatasetTestBase):
         writer.write_records(records)
 
         return [filename]
+
+    def _assert_same_ranks(self, expected_data, actual_data_iterator):
+        def __assert_same_ranks(expected_tensors, actual_tensors_iterator):
+            assert len(expected_tensors) == len(actual_tensors_iterator), \
+                "Expected {} pairs but " "got {} pairs".format(
+                    len(expected_tensors), len(actual_tensors_iterator))
+
+            for name, actual_tensor_iterator in actual_tensors_iterator.items():
+                assert name in expected_tensors, "Expected key {} be present in {}" \
+                    .format(name, actual_tensors_iterator.keys())
+                expected_tensor = expected_tensors[name]
+                # the shape is unknown for for parser key [%s]
+                if re.search(r"\[(.*?)\]", name):
+                    return
+                if isinstance(actual_tensor_iterator, sparse_tensor.SparseTensor):
+                    assert actual_tensor_iterator.shape.rank == expected_tensor.dense_shape.size
+                else:
+                    assert actual_tensor_iterator.shape.rank == len(expected_tensor.shape)
+
+        if isinstance(expected_data, tuple):
+            assert isinstance(actual_data_iterator, tuple), \
+                "Found type {} but expected type {}".format(type(actual_data_iterator),
+                                                            tuple)
+            assert len(expected_data) == 2, \
+                "Found {} components in expected dataset but must have {}" \
+                    .format(len(expected_data), 2)
+
+            assert len(actual_data_iterator) == 2, \
+                "Found {} components in actual dataset but expected {}" \
+                    .format(len(actual_data_iterator), 2)
+
+            expected_features, expected_labels = expected_data
+            actual_features_iter, actual_labels_iter = actual_data_iterator
+
+            __assert_same_ranks(expected_features, actual_features_iter)
+            __assert_same_ranks(expected_labels, actual_labels_iter)
+
+        else:
+            __assert_same_ranks(expected_data, actual_data_iterator)
 
     def _assert_same_data(self, expected_data, actual_data):
         if isinstance(expected_data, tuple):
@@ -94,7 +134,7 @@ class AvroDatasetTestBase(test_base.DatasetTestBase):
             "Expected {} pairs but " "got {} pairs".format(
                 len(expected_tensors), len(actual_tensors))
 
-        for name, actual_tensor in six.iteritems(actual_tensors):
+        for name, actual_tensor in actual_tensors.items():
             assert name in expected_tensors, "Expected key {} be present in {}" \
                 .format(name, actual_tensors.keys())
             expected_tensor = expected_tensors[name]
@@ -114,6 +154,8 @@ class AvroDatasetTestBase(test_base.DatasetTestBase):
             sess.run(iterator.initializer)
 
             for expected_datum in expected_data:
+                self._assert_same_ranks(expected_data=expected_datum,
+                                        actual_data_iterator=next_element)
                 # Tried to leverage assertAllSame or assertAllEqual but that does
                 # not handle string values properly
                 self._assert_same_data(expected_data=expected_datum,
