1648f3c788ef4b8fde71605fbe557216117b9d92
Yong Tang <yong.tang.github@outlook.com>
2019-12-03 09:52:31 -0800

    Enable SSE 4.2 and AVX on macOS as well, and further optimization (#649)
    * Enable SSE 4.2 and AVX on macOS as well
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Enable --compilation_mode=opt
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Stop after 1800 (30min) to populate Travis CI cache
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Re-enable
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Only enable SSE 4.2 and AVX, as we continue to have issues with Travis CI execution limit otherwise
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Enable all
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Update comment
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .travis/bazel.build.sh
+++ .travis/bazel.build.sh
@@ -57,7 +57,7 @@ if [[ "${BAZEL_CACHE}" != "" ]]; then
   args+=(--disk_cache=${BAZEL_CACHE})
 fi
 
-# Build with SSE4.2
+# Build with SSE4.2 and AVX
 if [[ "${BAZEL_CPU_OPTIMIZATION}" == "yes" ]]; then
   args+=(--copt=-msse4.2 --copt=-mavx)
 fi
