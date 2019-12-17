7fd96d0a3a7be391541aeef9c0605f10dcce6d48
Florian Raudies <florian.raudies@gmail.com>
2019-12-17 09:10:41 -0800

    Simplify the avro record dataset to make it one file to compile

--- tensorflow_io/avro/kernels/avro_record_dataset_op.cc
+++ tensorflow_io/avro/kernels/avro_record_dataset_op.cc
@@ -12,15 +12,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "tensorflow_io/avro/kernels/avro_record_dataset_op.h"
+#include "tensorflow/core/framework/common_shape_fns.h"
+#include "tensorflow/core/framework/dataset.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/lib/io/buffered_inputstream.h"
+#include "tensorflow/core/lib/io/inputbuffer.h"
+
+
 #include "tensorflow_io/avro/utils/avro_record_reader.h"
 #include "tensorflow_io/avro/utils/name_utils.h"
 // TODO(fraudies) with TF 2.0 switch to #include "tensorflow/core/kernels/data/name_utils.h"
-
-#include "tensorflow/core/framework/partial_tensor_shape.h"
-#include "tensorflow/core/framework/tensor.h"
-#include "tensorflow/core/lib/io/buffered_inputstream.h"
-#include "tensorflow/core/lib/io/inputbuffer.h"
 
 
 namespace tensorflow {
@@ -28,24 +31,48 @@ namespace data {
 
 // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/kernels/data/tf_record_dataset_op.cc
 
-/* static */ constexpr const char* const AvroRecordDatasetOp::kDatasetType;
-/* static */ constexpr const char* const AvroRecordDatasetOp::kFileNames;
-/* static */ constexpr const char* const AvroRecordDatasetOp::kBufferSize;
+constexpr char kDatasetType[] = "AvroRecordDataset";
+constexpr char kFileNames[] = "file_names";
+constexpr char kBufferSize[] = "buffer_size";
 
 constexpr char kCurrentFileIndex[] = "current_file_index";
 constexpr char kOffset[] = "offset";
 
-class AvroRecordDatasetOp::Dataset : public DatasetBase {
+class AvroRecordDatasetOp : public DatasetOpKernel {
  public:
-  explicit Dataset(OpKernelContext* ctx, std::vector<string> filenames,
+  AvroRecordDatasetOp(OpKernelContext* ctx, std::vector<string> filenames,
                    int64 buffer_size)
-      : DatasetBase(DatasetContext(ctx)),
+      : DatasetOpKernel(ctx),
         filenames_(std::move(filenames)),
         options_(AvroReaderOptions::CreateReaderOptions()) {
     if (buffer_size > 0) {
       options_.buffer_size = buffer_size;
     }
     VLOG(7) << "Created dataset with " << filenames_[0] << "... and buffer size " << buffer_size;
+  }
+
+  void MakeDataset(OpKernelContext* ctx, DatasetBase** output) {
+    const Tensor* filenames_tensor;
+    OP_REQUIRES_OK(ctx, ctx->input(kFileNames, &filenames_tensor));
+    OP_REQUIRES(
+        ctx, filenames_tensor->dims() <= 1,
+        errors::InvalidArgument("`filenames` must be a scalar or a vector."));
+
+    std::vector<string> filenames;
+    filenames.reserve(filenames_tensor->NumElements());
+    for (int i = 0; i < filenames_tensor->NumElements(); ++i) {
+      VLOG(2) << "Reading file: " << filenames_tensor->flat<string>()(i);
+      filenames.push_back(filenames_tensor->flat<string>()(i));
+    }
+
+    int64 buffer_size = -1;
+    OP_REQUIRES_OK(ctx,
+                   ParseScalarArgument<int64>(ctx, kBufferSize, &buffer_size));
+    OP_REQUIRES(ctx, buffer_size >= 0,
+                errors::InvalidArgument(
+                    "`buffer_size` must be >= 0 (0 == no buffering)"));
+    *output =
+        new Dataset(ctx, std::move(filenames), buffer_size);
   }
 
   std::unique_ptr<IteratorBase> MakeIteratorInternal(
@@ -203,39 +230,9 @@ class AvroRecordDatasetOp::Dataset : public DatasetBase {
   AvroReaderOptions options_;
 };
 
-AvroRecordDatasetOp::AvroRecordDatasetOp(OpKernelConstruction* ctx)
-    : DatasetOpKernel(ctx) {}
 
-void AvroRecordDatasetOp::MakeDataset(OpKernelContext* ctx,
-                                      DatasetBase** output) {
-  const Tensor* filenames_tensor;
-  OP_REQUIRES_OK(ctx, ctx->input(kFileNames, &filenames_tensor));
-  OP_REQUIRES(
-      ctx, filenames_tensor->dims() <= 1,
-      errors::InvalidArgument("`filenames` must be a scalar or a vector."));
-
-  std::vector<string> filenames;
-  filenames.reserve(filenames_tensor->NumElements());
-  for (int i = 0; i < filenames_tensor->NumElements(); ++i) {
-    VLOG(2) << "Reading file: " << filenames_tensor->flat<string>()(i);
-    filenames.push_back(filenames_tensor->flat<string>()(i));
-  }
-
-  int64 buffer_size = -1;
-  OP_REQUIRES_OK(ctx,
-                 ParseScalarArgument<int64>(ctx, kBufferSize, &buffer_size));
-  OP_REQUIRES(ctx, buffer_size >= 0,
-              errors::InvalidArgument(
-                  "`buffer_size` must be >= 0 (0 == no buffering)"));
-
-  *output =
-      new Dataset(ctx, std::move(filenames), buffer_size);
-}
-
-namespace {
 REGISTER_KERNEL_BUILDER(Name("AvroRecordDataset").Device(DEVICE_CPU),
                         AvroRecordDatasetOp);
-}
 
 }  // namespace data
 }  // namespace tensorflow
