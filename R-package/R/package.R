b63b3fb20fb0ee914f00e2ceb7f1ca6c875dbe03
Yuan Tang <terrytangyuan@gmail.com>
2019-12-18 12:33:43 -0500

    A couple fixes to CRAN check results in R package (#697)
    * A couple fixes to CRAN check results in R package
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Fix invalid URL and rebuild docs
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- R-package/R/package.R
+++ R-package/R/package.R
@@ -26,18 +26,24 @@ tfio_lib <- NULL
     environment = "r-tensorflow-io",
 
     on_load = function() {
-      check_tensorflow_version(displayed_warning)
+      if (!on_cran()) {
+        check_tensorflow_version(displayed_warning)
+      }
     },
 
     on_error = function(e) {
-      stop(tf_config()$error_message, call. = FALSE)
+      if (!on_cran()) {
+        stop(tf_config()$error_message, call. = FALSE)
+      }
     }
   )
 
-  if (!reticulate::py_module_available("tensorflow_io")) {
-    tfio_module_not_available_message()
-  } else {
-    tfio_lib <<- import("tensorflow_io", delay_load = delay_load)
+  if (!on_cran()) {
+    if (!reticulate::py_module_available("tensorflow_io")) {
+      tfio_module_not_available_message()
+    } else {
+      tfio_lib <<- import("tensorflow_io", delay_load = delay_load)
+    }
   }
 }
 
@@ -48,16 +54,22 @@ tfio_module_not_available_message <- function() {
 }
 
 check_tensorflow_version <- function(displayed_warning) {
-  current_tf_ver <- tf_version()
-  min_ver <- "1.13.0"
-  if (current_tf_ver < min_ver) {
-    if (!displayed_warning) {
-      packageStartupMessage(
-        "tfio requires TensorFlow version >= ", min_ver, " ",
-        "(you are currently running version ", current_tf_ver, ").\n")
-      displayed_warning <<- TRUE
+  if (!on_cran()) {
+    current_tf_ver <- tf_version()
+    min_ver <- "1.13.0"
+    if (current_tf_ver < min_ver) {
+      if (!displayed_warning) {
+        packageStartupMessage(
+          "tfio requires TensorFlow version >= ", min_ver, " ",
+          "(you are currently running version ", current_tf_ver, ").\n")
+        displayed_warning <<- TRUE
+      }
     }
   }
+}
+
+on_cran <- function() {
+  !identical(Sys.getenv("NOT_CRAN"), "true")
 }
 
 .onUnload <- function(libpath) {
