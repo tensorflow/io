2072ed20138c6c5620b7f710fec49432b430b0ec
Yong Tang <yong.tang.github@outlook.com>
2019-12-21 21:48:04 -0800

    Add apt-get install software-properties-common
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .github/workflows/build.wheel.sh
+++ .github/workflows/build.wheel.sh
@@ -22,6 +22,7 @@ fi
 if [[ $(uname) == "Linux" ]]; then
   apt-get -y -qq update
   if [[ "${PYTHON_VERSION}" == "python3.7" ]]; then
+    apt-get install -y -qq software-properties-common
     add-apt-repository -y ppa:deadsnakes/ppa
     apt-get -y -qq update
   fi
