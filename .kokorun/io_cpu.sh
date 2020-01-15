e457c13ee8b8bc6948189da1e305c6fa7fea9258
Yong Tang <yong.tang.github@outlook.com>
2020-01-15 07:08:49 -0800

    Fix path in Kokoro CPU build
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .kokorun/io_cpu.sh
+++ .kokorun/io_cpu.sh
@@ -62,7 +62,7 @@ docker run -i --rm --user $(id -u):$(id -g) -v /etc/password:/etc/password -v $P
 ls dist/*
 for f in dist/*.whl; do
   docker run -i --rm -v $PWD:/v -w /v --net=host \
-    quay.io/pypa/manylinux2010_x86_64 bash -x -e /v/third_party/tf/auditwheel repair --plat manylinux2010_x86_64 $f
+    quay.io/pypa/manylinux2010_x86_64 bash -x -e /v/tools/build/auditwheel repair --plat manylinux2010_x86_64 $f
 done
 
 sudo chown -R $(id -nu):$(id -ng) .
