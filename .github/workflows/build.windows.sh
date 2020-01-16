7e21eab42902b315293eb80dbe1ce7f17fae155c
Yong Tang <yong.tang.github@outlook.com>
2020-01-15 18:09:17 -0800

    Try
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .github/workflows/build.windows.sh
+++ .github/workflows/build.windows.sh
@@ -3,6 +3,9 @@ set -e -x
 python3 --version
 
 export MSYS2_ARG_CONV_EXCL="//"
+${BAZEL_PATH:=bazel} build -s --verbose_failures @zlib//:zlib
+${BAZEL_PATH:=bazel} build -s --verbose_failures @curl//:curl
+${BAZEL_PATH:=bazel} build -s --verbose_failures @com_github_azure_azure_storage_cpplite//:azure
 ${BAZEL_PATH:=bazel} build -s --verbose_failures //tensorflow_io/core:python/ops/libtensorflow_io.so
 
 exit $?
