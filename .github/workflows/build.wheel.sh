edc05adf033d35c68ffe8e3bdffd6b5d936982d5
Yong Tang <yong.tang.github@outlook.com>
2020-02-19 21:17:09 -0800

    Add the skil wheel install option
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .github/workflows/build.wheel.sh
+++ .github/workflows/build.wheel.sh
@@ -4,8 +4,11 @@ export TF_AZURE_USE_DEV_STORAGE=1
 
 run_test() {
   entry=$1
-  CPYTHON_VERSION=$($entry -c 'import sys; print(str(sys.version_info[0])+str(sys.version_info[1]))')
-  (cd wheelhouse && $entry -m pip install tensorflow_io-*-cp${CPYTHON_VERSION}-*.whl)
+  wheel=$2
+  if [[ "$wheel" == "yes" ]]; then
+    CPYTHON_VERSION=$($entry -c 'import sys; print(str(sys.version_info[0])+str(sys.version_info[1]))')
+    (cd wheelhouse && $entry -m pip install tensorflow_io-*-cp${CPYTHON_VERSION}-*.whl)
+  fi
   $entry -m pip install -q pytest pytest-benchmark boto3 python-dateutil==2.8.0 google-cloud-pubsub==0.39.1 pyarrow==0.16.0 pandas==0.24.2 scikit-learn==0.20.4 gast==0.2.2 google-cloud-bigquery-storage==0.7.0 google-cloud-bigquery==1.22.0 fastavro
   (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append $(find . -type f \( -iname "test_*.py" ! \( -iname "test_*_eager.py" -o -iname "test_gcs_config_ops.py" \) \)))
   (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append $(find . -type f \( -iname "test_*_eager.py" ! \( -iname "test_bigquery_eager.py" \) \)))
@@ -15,8 +18,13 @@ run_test() {
 }
 
 PYTHON_VERSION=python
+WHEEL_INSTALL=yes
 if [[ "$#" -gt 0 ]]; then
   PYTHON_VERSION="${1}"
+  if [[ "$#" -gt 1 ]]; then
+    WHEEL_INSTALL="${2}"
+    shift
+  fi
   shift
 fi
 if [[ $(uname) == "Linux" ]]; then
@@ -30,4 +38,4 @@ if [[ $(uname) == "Linux" ]]; then
   curl -sSOL https://bootstrap.pypa.io/get-pip.py
   $PYTHON_VERSION get-pip.py -q
 fi
-run_test $PYTHON_VERSION
+run_test $PYTHON_VERSION $WHEEL_INSTALL
