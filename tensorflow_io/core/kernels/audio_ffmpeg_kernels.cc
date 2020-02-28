7a19d3461a07929b4cad67734ccfba79d297674f
Yong Tang <yong.tang.github@outlook.com>
2020-02-28 09:08:53 -0800

    Add MP4A support for macOS with minimp4 and AVFoundation/Swift (#805)
    * Add MP4A support for macOS with minimp4 and AVFoundation
    
    This PR add initial mp4a support for macOS with minimp4
    for container parsing and macOS's native AVFoundation in swift
    for decoding.
    
    The swift code for calling AVFoundation is exported as __cdecl
    so that it could be used by C++/C.
    
    This only works on macOS.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Adjust Bazel BUILD file to allow windows and linux skip.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Properly close resources for MP3/MP4
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Fix a MP3 divide by zero issue, also prefix AAC buffer with ADTS, so that it could be parsed by FFmpeg
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Use FFmpeg on Linux
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Update to skip mp4 on Ubuntu 16.04
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

---
+++ tensorflow_io/core/kernels/audio_ffmpeg_kernels.cc
@@ -0,0 +1,221 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow_io/core/kernels/io_stream.h"
+
+extern "C" {
+
+#include <dlfcn.h>
+#include "libavcodec/avcodec.h"
+}
+
+namespace tensorflow {
+namespace data {
+void FFmpegInit();
+class DecodeAACFunctionState {
+ public:
+  DecodeAACFunctionState(const int64 codec)
+      : codec_parser_context_(nullptr, [](AVCodecParserContext* p) {
+          if (p != nullptr) {
+            av_parser_close(p);
+          }
+        }) {
+    codec_ = avcodec_find_decoder(AV_CODEC_ID_AAC);
+    if (codec_ != nullptr) {
+      AVCodecParserContext* codec_parser_context = av_parser_init(codec_->id);
+      if (codec_parser_context != nullptr) {
+        codec_parser_context_.reset(codec_parser_context);
+        return;
+      }
+      av_parser_close(codec_parser_context);
+    }
+  }
+  ~DecodeAACFunctionState() {}
+  bool Valid() {
+    if (codec_parser_context_.get() == nullptr) {
+      return false;
+    }
+    return true;
+  }
+  int64 Call(const int64 rate, const int64 channels, const void* data_in,
+             const int64 size_in, void* data_out, int64 size_out) {
+    std::unique_ptr<AVCodecContext, void (*)(AVCodecContext*)> codec_context(
+        nullptr, [](AVCodecContext* p) {
+          if (p != nullptr) {
+            avcodec_free_context(&p);
+          }
+        });
+    codec_context.reset(avcodec_alloc_context3(codec_));
+    if (codec_context.get() == nullptr) {
+      LOG(ERROR) << "unable to create codec context";
+      return -1;
+    }
+    codec_context->channels = channels;
+    codec_context->sample_rate = rate;
+    if (avcodec_open2(codec_context.get(), codec_, NULL) < 0) {
+      LOG(ERROR) << "unable to open codec context";
+      return -1;
+    }
+    std::unique_ptr<AVPacket, void (*)(AVPacket*)> packet(
+        nullptr, [](AVPacket* p) {
+          if (p != nullptr) {
+            av_packet_free(&p);
+          }
+        });
+    packet.reset(av_packet_alloc());
+    if (packet.get() == nullptr) {
+      LOG(ERROR) << "unable to create packet";
+      return -1;
+    }
+    std::unique_ptr<AVFrame, void (*)(AVFrame*)> frame(nullptr, [](AVFrame* p) {
+      if (p != nullptr) {
+        av_frame_free(&p);
+      }
+    });
+    frame.reset(av_frame_alloc());
+    if (frame.get() == nullptr) {
+      LOG(ERROR) << "unable to create frame";
+      return -1;
+    }
+    int ret =
+        av_parser_parse2(codec_parser_context_.get(), codec_context.get(),
+                         &packet->data, &packet->size, (const uint8_t*)data_in,
+                         size_in, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
+    if (ret < 0) {
+      LOG(ERROR) << "unable to parse: " << ret;
+      return ret;
+    }
+    if (ret != size_in) {
+      LOG(ERROR) << "size does not match: " << ret << " vs. " << size_in;
+      return -1;
+    }
+    int64 offset = 0;
+    if (packet->size > 0) {
+      ret = Decode(codec_context.get(), packet.get(), frame.get(), channels,
+                   data_out, size_out, &offset);
+      if (ret < 0) {
+        LOG(ERROR) << "unable to decode: " << ret;
+        return ret;
+      }
+    }
+    packet->data = nullptr;
+    packet->size = 0;
+    ret = Decode(codec_context.get(), packet.get(), frame.get(), channels,
+                 data_out, size_out, &offset);
+    if (ret < 0) {
+      LOG(ERROR) << "unable to decode and flush out: " << ret;
+      return ret;
+    }
+    if (offset != size_out) {
+      LOG(WARNING) << "output mismatch: " << offset << " vs. " << size_out
+                   << ret;
+    }
+    return 0;
+  }
+  int Decode(AVCodecContext* codec_context, AVPacket* packet, AVFrame* frame,
+             int64 channels, void* data_out, int64 size_out, int64* offset) {
+    int ret = avcodec_send_packet(codec_context, packet);
+    if (ret < 0) {
+      LOG(ERROR) << "unable to send packet: " << ret;
+      return ret;
+    }
+    while (ret >= 0) {
+      ret = avcodec_receive_frame(codec_context, frame);
+      if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+        return 0;
+      } else if (ret < 0) {
+        LOG(ERROR) << "unable to receive frame: " << ret;
+        return ret;
+      }
+      size_t data_size = av_get_bytes_per_sample(codec_context->sample_fmt);
+      if (codec_context->sample_fmt != AV_SAMPLE_FMT_FLTP) {
+        LOG(ERROR) << "format not supported: " << codec_context->sample_fmt;
+        return -1;
+      }
+      if (codec_context->channels != channels) {
+        LOG(ERROR) << "channels mismatch: " << codec_context->channels
+                   << " vs. " << channels;
+        ;
+        return -1;
+      }
+      if (data_size < 0) {
+        LOG(ERROR) << "unable to get data size: " << data_size;
+        return data_size;
+      }
+      if (((*offset) + frame->nb_samples * codec_context->channels *
+                           data_size) > size_out) {
+        LOG(WARNING) << "data out run: "
+                     << ((*offset) + frame->nb_samples *
+                                         codec_context->channels * data_size)
+                     << " vs. " << size_out;
+      }
+      for (int64 i = 0; i < frame->nb_samples; i++) {
+        for (int64 ch = 0; ch < codec_context->channels; ch++) {
+          if (((*offset) + data_size) <= size_out) {
+            memcpy((char*)data_out + (*offset), frame->data[ch] + data_size * i,
+                   data_size);
+          }
+          (*offset) += data_size;
+        }
+      }
+    }
+    return 0;
+  }
+
+ private:
+  AVCodec* codec_;
+  std::unique_ptr<AVCodecParserContext, void (*)(AVCodecParserContext*)>
+      codec_parser_context_;
+};
+
+}  // namespace data
+}  // namespace tensorflow
+
+extern "C" {
+
+void DecodeAACFunctionFiniFFmpeg(void* state) {
+  if (state != nullptr) {
+    delete static_cast<tensorflow::data::DecodeAACFunctionState*>(state);
+  }
+}
+
+void* DecodeAACFunctionInitFFmpeg(const int64_t codec, const int64_t rate,
+                                  const int64_t channels) {
+  tensorflow::data::FFmpegInit();
+
+  tensorflow::data::DecodeAACFunctionState* state =
+      new tensorflow::data::DecodeAACFunctionState(codec);
+  if (state != nullptr) {
+    if (state->Valid()) {
+      return state;
+    }
+    delete state;
+  }
+  return nullptr;
+}
+
+int64_t DecodeAACFunctionCallFFmpeg(void* state, const int64_t codec,
+                                    const int64_t rate, const int64_t channels,
+                                    const int64_t frames, const void* data_in,
+                                    int64_t size_in, void* data_out,
+                                    int64_t size_out) {
+  if (state != nullptr) {
+    return static_cast<tensorflow::data::DecodeAACFunctionState*>(state)->Call(
+        rate, channels, data_in, size_in, data_out, size_out);
+  }
+  return -1;
+}
+}
---
+++ tensorflow_io/core/kernels/audio_wav_kernels.cc
@@ -0,0 +1,314 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow_io/core/kernels/audio_kernels.h"
+
+namespace tensorflow {
+namespace data {
+namespace {
+
+// See http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/WAVE.html
+struct WAVHeader {
+  char riff[4];           // RIFF Chunk ID: "RIFF"
+  int32 riff_size;        // RIFF chunk size: 4 + n (file size - 8)
+  char wave[4];           // WAVE ID: "WAVE"
+  char fmt[4];            // fmt Chunk ID: "fmt "
+  int32 fmt_size;         // fmt Chunk size: 16, 18, or 40
+  int16 wFormatTag;       // Format code: WAVE_FORMAT_PCM (1) for PCM.
+                          // WAVE_FORMAT_EXTENSIBLE (0xFFFE) for SubFormat
+  int16 nChannels;        // Number of channels
+  int32 nSamplesPerSec;   // Sampling rate
+  int32 nAvgBytesPerSec;  // Data rate
+  int16 nBlockAlign;      // Data block size (bytes)
+  int16 wBitsPerSample;   // Bits per sample
+};
+
+struct ExtensionHeader {
+  int16 cbSize;               // Size of the extension (0 or 22)
+  int16 wValidBitsPerSample;  // Number of valid bits
+  int32 dwChannelMask;        // Speaker position mask
+  char SubFormat[16];         // GUID
+};
+
+struct DataHeader {
+  char mark[4];
+  int32 size;
+};
+Status ValidateWAVHeader(struct WAVHeader* header) {
+  if (memcmp(header->riff, "RIFF", 4) != 0) {
+    return errors::InvalidArgument("WAV file must starts with `RIFF`");
+  }
+  if (memcmp(header->wave, "WAVE", 4) != 0) {
+    return errors::InvalidArgument("WAV file must contains riff type `WAVE`");
+  }
+  if (memcmp(header->fmt, "fmt ", 4) != 0) {
+    return errors::InvalidArgument("WAV file must contains `fmt ` mark");
+  }
+  if (header->fmt_size != 16 && header->fmt_size != 18 &&
+      header->fmt_size != 40) {
+    return errors::InvalidArgument(
+        "WAV file must have `fmt_size ` 16, 18, or 40, received: ",
+        header->fmt_size);
+  }
+  if (header->wFormatTag != 1 &&
+      header->wFormatTag != static_cast<int16>(0xFFFE)) {
+    return errors::InvalidArgument(
+        "WAV file must have `wFormatTag` 1 or 0xFFFE, received: ",
+        header->wFormatTag);
+  }
+  if (header->nChannels <= 0) {
+    return errors::InvalidArgument("WAV file have invalide channels: ",
+                                   header->nChannels);
+  }
+  return Status::OK();
+}
+
+class WAVReadableResource : public AudioReadableResourceBase {
+ public:
+  WAVReadableResource(Env* env) : env_(env) {}
+  ~WAVReadableResource() {}
+
+  Status Init(const string& input) override {
+    mutex_lock l(mu_);
+    const string& filename = input;
+    file_.reset(new SizedRandomAccessFile(env_, filename, nullptr, 0));
+    TF_RETURN_IF_ERROR(file_->GetFileSize(&file_size_));
+
+    StringPiece result;
+    TF_RETURN_IF_ERROR(
+        file_->Read(0, sizeof(header_), &result, (char*)(&header_)));
+    header_length_ = sizeof(header_);
+    int64 fmt_position = 12;
+    while (memcmp(header_.fmt, "fmt ", 4) != 0) {
+      // Skip JUNK/bext/etc field.
+      if (memcmp(header_.fmt, "JUNK", 4) != 0 &&
+          memcmp(header_.fmt, "bext", 4) != 0 &&
+          memcmp(header_.fmt, "iXML", 4) != 0 &&
+          memcmp(header_.fmt, "qlty", 4) != 0 &&
+          memcmp(header_.fmt, "mext", 4) != 0 &&
+          memcmp(header_.fmt, "levl", 4) != 0 &&
+          memcmp(header_.fmt, "link", 4) != 0 &&
+          memcmp(header_.fmt, "axml", 4) != 0) {
+        return errors::InvalidArgument("unexpected field: ", header_.fmt);
+      }
+      int32 size_of_chunk = 4 + 4 + header_.fmt_size;
+      if (header_.fmt_size % 2 == 1) {
+        size_of_chunk += 1;
+      }
+      fmt_position += size_of_chunk;
+      // Re-read the header
+      TF_RETURN_IF_ERROR(file_->Read(fmt_position, sizeof(header_) - 12,
+                                     &result, (char*)(&header_) + 12));
+      header_length_ = fmt_position + sizeof(header_) - 12;
+    }
+
+    TF_RETURN_IF_ERROR(ValidateWAVHeader(&header_));
+    if (header_.riff_size + 8 != file_size_) {
+      // corrupted file?
+    }
+    int64 filesize = header_.riff_size + 8;
+    int64 position = header_length_ + header_.fmt_size - 16;
+
+    int64 nSamples = 0;
+    do {
+      struct DataHeader head;
+      TF_RETURN_IF_ERROR(
+          file_->Read(position, sizeof(head), &result, (char*)(&head)));
+      position += result.size();
+      if (memcmp(head.mark, "data", 4) == 0) {
+        // Data should be block aligned
+        // bytes = nSamples * nBlockAlign
+        if (head.size % header_.nBlockAlign != 0) {
+          return errors::InvalidArgument("data chunk should be block aligned (",
+                                         header_.nBlockAlign,
+                                         "), received: ", head.size);
+        }
+        nSamples += head.size / header_.nBlockAlign;
+      }
+      position += head.size;
+    } while (position < filesize);
+
+    switch (header_.wBitsPerSample) {
+      case 8:
+        dtype_ = DT_INT8;
+        break;
+      case 16:
+        dtype_ = DT_INT16;
+        break;
+      case 24:
+        dtype_ = DT_INT32;
+        break;
+      default:
+        return errors::InvalidArgument("unsupported wBitsPerSample: ",
+                                       header_.wBitsPerSample);
+    }
+
+    shape_ = TensorShape({nSamples, header_.nChannels});
+
+    rate_ = header_.nSamplesPerSec;
+
+    return Status::OK();
+  }
+
+  Status Spec(TensorShape* shape, DataType* dtype, int32* rate) override {
+    mutex_lock l(mu_);
+    *shape = shape_;
+    *dtype = dtype_;
+    *rate = rate_;
+    return Status::OK();
+  }
+
+  Status Read(const int64 start, const int64 stop,
+              std::function<Status(const TensorShape& shape, Tensor** value)>
+                  allocate_func) override {
+    mutex_lock l(mu_);
+    int64 sample_stop =
+        (stop < 0) ? (shape_.dim_size(0))
+                   : (stop < shape_.dim_size(0) ? stop : shape_.dim_size(0));
+    int64 sample_start = (start >= sample_stop) ? sample_stop : start;
+
+    Tensor* value;
+    TF_RETURN_IF_ERROR(allocate_func(
+        TensorShape({sample_stop - sample_start, shape_.dim_size(1)}), &value));
+
+    int64 sample_offset = 0;
+    if (header_.riff_size + 8 != file_size_) {
+      // corrupted file?
+    }
+    int64 filesize = header_.riff_size + 8;
+    int64 position = header_length_ + header_.fmt_size - 16;
+    do {
+      StringPiece result;
+      struct DataHeader head;
+      TF_RETURN_IF_ERROR(
+          file_->Read(position, sizeof(head), &result, (char*)(&head)));
+      position += result.size();
+      if (memcmp(head.mark, "data", 4) == 0) {
+        // Already checked the alignment
+        int64 block_sample_start = sample_offset;
+        int64 block_sample_stop =
+            sample_offset + head.size / header_.nBlockAlign;
+        // only read if block_sample_start and block_sample_stop within range
+        if (sample_start < block_sample_stop &&
+            sample_stop > block_sample_start) {
+          int64 read_sample_start =
+              (block_sample_start > sample_start ? block_sample_start
+                                                 : sample_start);
+          int64 read_sample_stop =
+              (block_sample_stop < sample_stop ? block_sample_stop
+                                               : sample_stop);
+          int64 read_bytes_start =
+              position +
+              (read_sample_start - block_sample_start) * header_.nBlockAlign;
+          int64 read_bytes_stop =
+              position +
+              (read_sample_stop - block_sample_start) * header_.nBlockAlign;
+          string buffer;
+          buffer.resize(read_bytes_stop - read_bytes_start);
+          TF_RETURN_IF_ERROR(file_->Read(read_bytes_start,
+                                         read_bytes_stop - read_bytes_start,
+                                         &result, &buffer[0]));
+          switch (header_.wBitsPerSample) {
+            case 8:
+              if (header_.wBitsPerSample * header_.nChannels !=
+                  header_.nBlockAlign * 8) {
+                return errors::InvalidArgument(
+                    "unsupported wBitsPerSample and header.nBlockAlign: ",
+                    header_.wBitsPerSample, ", ", header_.nBlockAlign);
+              }
+              memcpy((char*)(value->flat<int8>().data()) +
+                         ((read_sample_start - sample_start) *
+                          header_.nBlockAlign),
+                     &buffer[0], (read_bytes_stop - read_bytes_start));
+              break;
+            case 16:
+              if (header_.wBitsPerSample * header_.nChannels !=
+                  header_.nBlockAlign * 8) {
+                return errors::InvalidArgument(
+                    "unsupported wBitsPerSample and header.nBlockAlign: ",
+                    header_.wBitsPerSample, ", ", header_.nBlockAlign);
+              }
+              memcpy((char*)(value->flat<int16>().data()) +
+                         ((read_sample_start - sample_start) *
+                          header_.nBlockAlign),
+                     &buffer[0], (read_bytes_stop - read_bytes_start));
+              break;
+            case 24:
+              // NOTE: The conversion is from signed integer 24 to signed
+              // integer 32 (left shift 8 bits)
+              if (header_.wBitsPerSample * header_.nChannels !=
+                  header_.nBlockAlign * 8) {
+                return errors::InvalidArgument(
+                    "unsupported wBitsPerSample and header.nBlockAlign: ",
+                    header_.wBitsPerSample, ", ", header_.nBlockAlign);
+              }
+              for (int64 i = read_sample_start; i < read_sample_stop; i++) {
+                for (int64 j = 0; j < header_.nChannels; j++) {
+                  char* data_p =
+                      (char*)(value->flat<int32>().data() +
+                              ((i - sample_start) * header_.nChannels + j));
+                  char* read_p = (char*)(&buffer[((i - read_sample_start) *
+                                                  header_.nBlockAlign)]) +
+                                 3 * j;
+                  data_p[3] = read_p[2];
+                  data_p[2] = read_p[1];
+                  data_p[1] = read_p[0];
+                  data_p[0] = 0x00;
+                }
+              }
+              break;
+            default:
+              return errors::InvalidArgument(
+                  "unsupported wBitsPerSample and header.nBlockAlign: ",
+                  header_.wBitsPerSample, ", ", header_.nBlockAlign);
+          }
+        }
+        sample_offset = block_sample_stop;
+      }
+      position += head.size;
+    } while (position < filesize);
+
+    return Status::OK();
+  }
+  string DebugString() const override { return "WAVReadableResource"; }
+
+ private:
+  mutable mutex mu_;
+  Env* env_ GUARDED_BY(mu_);
+  std::unique_ptr<SizedRandomAccessFile> file_ GUARDED_BY(mu_);
+  uint64 file_size_ GUARDED_BY(mu_);
+  DataType dtype_;
+  TensorShape shape_;
+  int64 rate_;
+
+  struct WAVHeader header_;
+  int64 header_length_;
+};
+
+}  // namespace
+
+Status WAVReadableResourceInit(
+    Env* env, const string& input,
+    std::unique_ptr<AudioReadableResourceBase>& resource) {
+  resource.reset(new WAVReadableResource(env));
+  Status status = resource->Init(input);
+  if (!status.ok()) {
+    resource.reset(nullptr);
+  }
+  return status;
+}
+
+}  // namespace data
+}  // namespace tensorflow
