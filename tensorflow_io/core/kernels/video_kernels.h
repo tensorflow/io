16140a2a64a3ef4ee7db2c4b423516813728cc55
Yong Tang <yong.tang.github@outlook.com>
2020-03-03 09:16:04 -0800

    Add Video Capture Support for macOS through AVFoundation/Swift (#821)
    * Add Video Capture Support for macOS through AVFoundation/Swift
    
    This PR is part of the effort in resolving 814.
    
    In 814, the feature request is to add video capture support for Linux,
    likely through Video4Linux.
    
    Due to some limitations Video4Linux will need a compatible USB camera first.
    
    This PR, instead tries to resolve the featue requrest on macOS first.
    
    On macOS the built-in camera could be accessed through AVFoundation's Swift API.
    This PR uses Swift to access  AVCaptureSession/etc, and exported to C function (`cdecl`)
    so that it could be used in C++ kernel in tensorflow-io.
    
    Since macOS's raw video capture format is NV12 (kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange)
    additional work is needed to convert NV12 into RGB format, so that a whole pipeline could be built
    up to allow using video capture for tf.keras' inference.
    
    This PR does not resolve the NV12 => RGB yet. Will address in separate PRs.
    
    Also, since video capture is technically a continuous stream and is not repeatable,
    it is not possible to train based on video capture with multiple epochs.
    
    Finally, the following is a sample usage which takes video capture and saves as nv12 raw file.
    
    The NV12 raw file could be checked by using ffmpeg to convert to JPEG to validate.
    
    Note: the following is a validation
    YUV image could be converted to JPEG with:
    ```
    ffmpeg -s 1280x720 -pix_fmt nv12 -i frame_{i}.yuv frame_{i}.jpg
    ```
    
    Usage:
    ```
    dataset = tfio.experimental.IODataset.stream().from_video_capture(
        "device").take(5)
    i = 0
    for frame in dataset:
      print("Frame {}: shape({}) dtype({}) length({})".format(
          i, frame.shape, frame.dtype, tf.strings.length(frame)))
      tf.io.write_file("frame_{}.yuv".format(i), frame)
      i += 1
    ```
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Add Video4Linux V2 support on Linux
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Update to use device name in API calls
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Fix typo in Windows
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Fix test typo
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

---
+++ tensorflow_io/core/kernels/video_kernels.h
@@ -0,0 +1,183 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/framework/resource_mgr.h"
+#include "tensorflow/core/framework/resource_op_kernel.h"
+
+#if defined(__linux__)
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <linux/videodev2.h>
+
+static int xioctl(int fh, int request, void* arg) {
+  int r;
+
+  do {
+    r = ioctl(fh, request, arg);
+  } while (-1 == r && EINTR == errno);
+
+  return r;
+}
+namespace tensorflow {
+namespace data {
+
+class VideoCaptureContext {
+ public:
+  VideoCaptureContext()
+      : context_(nullptr,
+                 [](void* p) {
+                   if (p != nullptr) {
+                     free(p);
+                   }
+                 }),
+        fd_scope_(nullptr, [](int* p) {
+          if (p != nullptr) {
+            close(*p);
+          }
+        }) {}
+  ~VideoCaptureContext() {}
+
+  Status Init(const string& device, int64_t* bytes, int64_t* width,
+              int64_t* height) {
+    device_ = device;
+
+    const char* devname = device.c_str();
+    struct stat st;
+    if (-1 == stat(devname, &st)) {
+      return errors::InvalidArgument("cannot identify '", devname, "': ", errno,
+                                     ", ", strerror(errno));
+    }
+
+    if (!S_ISCHR(st.st_mode)) {
+      return errors::InvalidArgument(devname, " is no device");
+    }
+
+    fd_ = open(devname, O_RDWR /* required */ | O_NONBLOCK, 0);
+    if (-1 == fd_) {
+      return errors::InvalidArgument("cannot open '", devname, "': ", errno,
+                                     ", ", strerror(errno));
+    }
+    fd_scope_.reset(&fd_);
+
+    struct v4l2_capability cap;
+    if (-1 == xioctl(fd_, VIDIOC_QUERYCAP, &cap)) {
+      if (EINVAL == errno) {
+        return errors::InvalidArgument(devname, " is no V4L2 device");
+      } else {
+        return errors::InvalidArgument("cannot VIDIOC_QUERYCAP '", devname,
+                                       "': ", errno, ", ", strerror(errno));
+      }
+    }
+
+    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+      return errors::InvalidArgument(devname, " is no video capture device");
+    }
+
+    if (!(cap.capabilities & V4L2_CAP_READWRITE)) {
+      return errors::InvalidArgument(devname, " does not support read i/o");
+    }
+
+    struct v4l2_format fmt;
+    memset(&(fmt), 0, sizeof(fmt));
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (-1 == xioctl(fd_, VIDIOC_G_FMT, &fmt)) {
+      return errors::InvalidArgument("cannot VIDIOC_G_FMT '", devname,
+                                     "': ", errno, ", ", strerror(errno));
+    }
+
+    /* Buggy driver paranoia. */
+    {
+      unsigned int min;
+      min = fmt.fmt.pix.width * 2;
+      if (fmt.fmt.pix.bytesperline < min) {
+        fmt.fmt.pix.bytesperline = min;
+      }
+      min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
+      if (fmt.fmt.pix.sizeimage < min) {
+        fmt.fmt.pix.sizeimage = min;
+      }
+    }
+
+    if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_YUYV) {
+      return errors::InvalidArgument(
+          "only V4L2_PIX_FMT_YUYV is supported, received ",
+          fmt.fmt.pix.pixelformat);
+    }
+
+    *bytes = fmt.fmt.pix.sizeimage;
+    *width = fmt.fmt.pix.width;
+    *height = fmt.fmt.pix.height;
+
+    return Status::OK();
+  }
+  Status Read(void* data, size_t size) {
+    do {
+      fd_set fds;
+      struct timeval tv;
+      int r;
+
+      FD_ZERO(&fds);
+      FD_SET(fd_, &fds);
+
+      /* Timeout. */
+      tv.tv_sec = 2;
+      tv.tv_usec = 0;
+      r = select(fd_ + 1, &fds, NULL, NULL, &tv);
+
+      if (-1 == r) {
+        if (EINTR == errno) {
+          continue;
+        }
+        return errors::InvalidArgument("cannot select: ", errno, ", ",
+                                       strerror(errno));
+      }
+      if (0 == r) {
+        return errors::InvalidArgument("select timeout");
+      }
+
+      if (-1 == read(fd_, data, size)) {
+        if (EAGAIN == errno) {
+          /* EAGAIN - continue select loop. */
+          continue;
+        }
+        if (EIO == errno) {
+          /* Could ignore EIO, see spec. */
+          /* fall through */
+        }
+        return errors::InvalidArgument("cannot read: ", errno, ", ",
+                                       strerror(errno));
+      }
+      // Data Obtained, break
+      break;
+    } while (true);
+    return Status::OK();
+  }
+
+ protected:
+  mutable mutex mu_;
+
+  std::unique_ptr<void, void (*)(void*)> context_;
+  std::unique_ptr<int, void (*)(int*)> fd_scope_;
+  string device_;
+  int fd_;
+};
+
+}  // namespace data
+}  // namespace tensorflow
+#endif
---
+++ tensorflow_io/core/ops/video_ops.cc
@@ -0,0 +1,45 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/framework/common_shape_fns.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+
+namespace tensorflow {
+namespace io {
+namespace {
+
+REGISTER_OP("IO>VideoCaptureReadableInit")
+    .Input("input: string")
+    .Output("resource: resource")
+    .Attr("container: string = ''")
+    .Attr("shared_name: string = ''")
+    .SetShapeFn([](shape_inference::InferenceContext* c) {
+      c->set_output(0, c->Scalar());
+      return Status::OK();
+    });
+
+REGISTER_OP("IO>VideoCaptureReadableRead")
+    .Input("input: resource")
+    .Input("index: int64")
+    .Output("value: string")
+    .SetShapeFn([](shape_inference::InferenceContext* c) {
+      c->set_output(0, c->MakeShape({c->UnknownDim()}));
+      return Status::OK();
+    });
+
+}  // namespace
+}  // namespace io
+}  // namespace tensorflow
