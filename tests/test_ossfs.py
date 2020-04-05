a9cc9806cdcc00ada49b7026c61b17d279275453
Zou Xu <zouxu09@gmail.com>
2020-04-05 08:58:16 +0000

    Fix issue: fail to use tf.gfile.Rename to rename oss directory (#834)
    The problem:
       When there's trailling '/' in directory path, the Rename function incorrectly put two '/'s in the path, which in oss is considered as a differecnt file (thus a 404 NotFound error is reported)
    
    The fix is to correctly handle the url join

--- tests/test_ossfs.py
+++ tests/test_ossfs.py
@@ -140,6 +140,23 @@ class OSSFSTest(test.TestCase):
         self.assertEqual(content, content_s)
         self.assertIn("d2", content)
 
+        # Test Rename non-empty directories
+        not_empty_dir = get_oss_path("not_empty_dir/")
+        rename_not_empty_dir = get_oss_path("rename_not_empty_dir/")
+        gfile.MakeDirs(not_empty_dir)
+        not_empty_file = get_oss_path("not_empty_dir/not_empty_file")
+        rename_not_empty_file = get_oss_path("rename_not_empty_dir/not_empty_file")
+        with gfile.Open(not_empty_file, mode="w") as fh:
+            content = "file content"
+            fh.write(content)
+        self.assertTrue(gfile.Exists(not_empty_dir))
+        self.assertTrue(gfile.Exists(not_empty_file))
+        gfile.Rename(not_empty_dir, rename_not_empty_dir, overwrite=True)
+        self.assertFalse(gfile.Exists(not_empty_dir))
+        self.assertFalse(gfile.Exists(not_empty_file))
+        self.assertTrue(gfile.Exists(rename_not_empty_dir))
+        self.assertTrue(gfile.Exists(rename_not_empty_file))
+
 
 if __name__ == "__main__":
     test.main()
