61b9731ab4f8460d057104612b9c5c9cca11887e
Yong Tang <yong.tang.github@outlook.com>
2020-04-17 18:59:10 -0700

    Free up disk space on Linux (#903)
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

---
+++ .github/workflows/build.space.sh
@@ -0,0 +1,8 @@
+# Free disk space on Linux
+        sudo swapoff /swapfile
+        sudo rm -rf /swapfile /usr/share/dotnet /usr/local/lib/android /opt/ghc
+        sudo apt-get remove php* ruby-* subversion mongodb-org -yq >/dev/null 2>&1
+        sudo apt-get autoremove -y >/dev/null 2>&1
+        sudo apt-get autoclean -y >/dev/null 2>&1
+        sudo rm -rf /usr/local/lib/android >/dev/null 2>&1
+        docker rmi $(docker image ls -aq) >/dev/null 2>&1
