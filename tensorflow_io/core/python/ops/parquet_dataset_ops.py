b44095f99c9fb63839ac51b6ed210f35dd89e9d1
Yong Tang <yong.tang.github@outlook.com>
2020-05-14 12:35:13 -0700

    Add graph mode support for Parquet Dataset (#960)
    * Add graph mode support for Parquet Dataset
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Keep name consistent
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Empty push for CI
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/core/python/ops/parquet_dataset_ops.py
+++ tensorflow_io/core/python/ops/parquet_dataset_ops.py
@@ -14,90 +14,98 @@
 # ==============================================================================
 """ParquetDataset"""
 
-import sys
-import uuid
 import collections
 
 import tensorflow as tf
 from tensorflow_io.core.python.ops import core_ops
 
 
-class _ParquetIODatasetFunction:
-    def __init__(self, function, resource, component, shape, dtype):
-        self._function = function
-        self._resource = resource
-        self._component = component
-        self._shape = tf.TensorShape([None]).concatenate(shape[1:])
-        self._dtype = dtype
-
-    def __call__(self, start, stop):
-        return self._function(
-            self._resource,
-            start=start,
-            stop=stop,
-            component=self._component,
-            shape=self._shape,
-            dtype=self._dtype,
-        )
-
-
-class ParquetIODataset(tf.compat.v2.data.Dataset):
+class ParquetIODataset(tf.data.Dataset):
     """ParquetIODataset"""
 
     def __init__(self, filename, columns=None, internal=True):
         """ParquetIODataset."""
-        if not internal:
-            raise ValueError(
-                "ParquetIODataset constructor is private; "
-                "please use one "
-                "of the factory methods instead (e.g., "
-                "IODataset.from_parquet())"
+        assert internal
+        with tf.name_scope("ParquetIODataset"):
+            components, shapes, dtypes = core_ops.io_parquet_readable_info(
+                filename, shared=filename, container="ParquetIODataset"
             )
-        with tf.name_scope("ParquetIODataset") as scope:
-            capacity = 4096
 
-            resource, columns_v = core_ops.io_parquet_readable_init(
-                filename,
-                container=scope,
-                shared_name="{}/{}".format(filename, uuid.uuid4().hex),
-            )
-            columns = columns if columns is not None else columns_v.numpy()
-
-            columns_dataset = []
-
-            columns_function = []
-            for column in columns:
-                shape, dtype = core_ops.io_parquet_readable_spec(resource, column)
-                shape = tf.TensorShape([None if e < 0 else e for e in shape.numpy()])
-                dtype = tf.as_dtype(dtype.numpy())
-                function = _ParquetIODatasetFunction(
-                    core_ops.io_parquet_readable_read, resource, column, shape, dtype
-                )
-                columns_function.append(function)
-
-            for (column, function) in zip(columns, columns_function):
-                column_dataset = tf.compat.v2.data.Dataset.range(
-                    0, sys.maxsize, capacity
-                )
-                column_dataset = column_dataset.map(
-                    lambda index: function(index, index + capacity)
-                )
-                column_dataset = column_dataset.apply(
-                    tf.data.experimental.take_while(
-                        lambda v: tf.greater(tf.shape(v)[0], 0)
+            if not tf.executing_eagerly():
+                assert columns is not None
+                assert isinstance(columns, dict)
+                dtypes = [
+                    spec if isinstance(spec, tf.dtypes.DType) else spec.dtype
+                    for column, spec in columns.items()
+                ]
+                columns = list(columns.keys())
+            else:
+                columns = (
+                    None
+                    if columns is None
+                    else (
+                        list(columns.keys()) if isinstance(columns, dict) else columns
                     )
                 )
-                columns_dataset.append(column_dataset)
-            if len(columns_dataset) == 1:
-                dataset = columns_dataset[0]
-            else:
-                dataset = tf.compat.v2.data.Dataset.zip(
-                    collections.OrderedDict(list(zip(columns, columns_dataset)))
-                )
-            dataset = dataset.unbatch()
 
-            self._function = columns_function
-            self._dataset = dataset
+            def shape_f(shapes, components, column):
+                shape = tf.boolean_mask(shapes, tf.math.equal(components, column))[0]
+                shape = tf.boolean_mask(shape, tf.math.greater_equal(shape, 0))
+                return shape
+
+            def dtype_f(dtypes, components, column):
+                dtype = tf.boolean_mask(dtypes, tf.math.equal(components, column))[0]
+                dtype = tf.as_dtype(dtype.numpy())
+                return dtype
+
+            if columns is not None:
+                shapes = [shape_f(shapes, components, column) for column in columns]
+                if tf.executing_eagerly():
+                    dtypes = [dtype_f(dtypes, components, column) for column in columns]
+                components = columns
+            else:
+                shapes = tf.unstack(shapes)
+                dtypes = [tf.as_dtype(dtype.numpy()) for dtype in tf.unstack(dtypes)]
+                components = [component.numpy() for component in tf.unstack(components)]
+
+            self._filename = filename
+            self._components = components
+            self._shapes = shapes
+            self._dtypes = dtypes
+
+            def dataset_f(component, shape, dtype):
+                step = 4096
+                indices_start = tf.data.Dataset.range(0, shape[0], step)
+                indices_stop = indices_start.skip(1).concatenate(
+                    tf.data.Dataset.from_tensor_slices([shape[0]])
+                )
+                dataset = tf.data.Dataset.zip((indices_start, indices_stop))
+
+                def f(start, stop):
+                    return core_ops.io_parquet_readable_read(
+                        input=self._filename,
+                        shared=self._filename,
+                        component=component,
+                        shape=shape,
+                        start=start,
+                        stop=stop,
+                        dtype=dtype,
+                        container="ParquetIODataset",
+                    )
+
+                dataset = dataset.map(f)
+                dataset = dataset.unbatch()
+                return dataset
+
+            entries = list(zip(components, shapes, dtypes))
+            datasets = [
+                dataset_f(component, shape, dtype)
+                for component, shape, dtype in entries
+            ]
+            self._dataset = tf.data.Dataset.zip(
+                collections.OrderedDict(list(zip(components, datasets)))
+            )
+
             super().__init__(
                 self._dataset._variant_tensor
             )  # pylint: disable=protected-access
