0885837756a728ef2c3ae80b3e7c211c6550eaa3
marioecd <marioecd@google.com>
2020-05-21 22:37:02 -0700

    Adding memcached file system and cache to gstpufs (#979)
    * Adding memcached_file_block_cache to gstpufs
    
    PiperOrigin-RevId: 310431739
    
    * Add farmhash_archive

---
+++ tensorflow_io/core/kernels/gstpufs/gce_memcached_server_list_provider.h
@@ -0,0 +1,33 @@
+#ifndef TENSORFLOW_IO_GSTPUFS_GCE_MEMCACHED_SERVER_LIST_PROVIDER_H_  // NOLINT
+#define TENSORFLOW_IO_GSTPUFS_GCE_MEMCACHED_SERVER_LIST_PROVIDER_H_  // NOLINT
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/memory/memory.h"
+#include "tensorflow/core/lib/core/status.h"
+#include "tensorflow/core/platform/cloud/compute_engine_metadata_client.h"
+
+namespace tensorflow {
+
+class GceMemcachedServerListProvider {
+ public:
+  explicit GceMemcachedServerListProvider(
+      std::shared_ptr<ComputeEngineMetadataClient> metadata_client);
+  virtual ~GceMemcachedServerListProvider();
+
+  Status GetServerList(std::vector<string>* server_list);
+
+  void SetMetadataClient(
+      std::shared_ptr<ComputeEngineMetadataClient> metadata_client);
+
+ private:
+  std::shared_ptr<ComputeEngineMetadataClient> google_metadata_client_;
+  std::vector<string> cached_list_;
+  TF_DISALLOW_COPY_AND_ASSIGN(GceMemcachedServerListProvider);
+};
+
+}  // namespace tensorflow
+
+#endif  // TENSORFLOW_IO_GSTPUFS_GCE_MEMCACHED_SERVER_LIST_PROVIDER_H_
