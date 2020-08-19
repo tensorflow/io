b564d17a85a8fc6694a0685ffa8d622dac45b61f
Vignesh Kothapalli <vikoth18@in.ibm.com>
2020-08-20 03:50:23 +0530

    added 'import' tests and github actions for docker images (#1090)
    * added basic tests and github actions
    
    * removed -i option from docker run during tests
    
    * updated docker test setup for mac and win
    
    * removed win docker test
    
    * modified docker tests on mac
    
    * removed docker tests on macos
    
    * added docker tests on macOS
    
    * removed docker devel test in macos

---
+++ tools/docker/tests/bazel_build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+echo "Configuring the environment for build ..."
+./configure.sh
+
+echo "Cleaning up existing bazel build files ..."
+rm -rf bazel-*
+
+echo "Building the tensorflow-io package ..."
+bazel build -j 8 --copt=-msse4.2 --copt=-mavx --compilation_mode=opt --verbose_failures --test_output=errors --crosstool_top=//third_party/toolchains/gcc7_manylinux2010:toolchain //tensorflow_io/...
+
+echo "Validating import ..."
+python -c "import tensorflow_io as tfio; print(tfio.__version__)"
