6668dd783128378fb36d0dc90d7dca361b444ed3
Vignesh Kothapalli <vikoth18@in.ibm.com>
2020-09-26 23:24:23 +0530

    fix auto.offset.reset bug while consuming messages using KafkaGroupIODataset (#1134)
    * fix auto.offset.reset bug while consuming messages
    
    * update docstrings

--- tensorflow_io/core/python/experimental/kafka_batch_io_dataset_ops.py
+++ tensorflow_io/core/python/experimental/kafka_batch_io_dataset_ops.py
@@ -38,7 +38,12 @@ class KafkaBatchIODataset(tf.data.Dataset):
     >>> dataset = tfio.experimental.streaming.KafkaBatchIODataset(
                         topics=["topic1"],
                         group_id="cg",
-                        servers="localhost:9092"
+                        servers="localhost:9092",
+                        configuration=[
+                            "session.timeout.ms=7000",
+                            "max.poll.interval.ms=8000",
+                            "auto.offset.reset=earliest",
+                        ],
                     )
 
     >>> for mini_batch in dataset:
@@ -46,7 +51,11 @@ class KafkaBatchIODataset(tf.data.Dataset):
     ...            lambda m, k: (tf.cast(m, tf.float32), tf.cast(k, tf.float32)))
 
     Since `mini_batch` is of type `tf.data.Dataset` we can perform all the operations that it
-    inherits from `tf.data.Dataset`.
+    inherits from `tf.data.Dataset`. Also, the `auto.offset.reset` configuration is set to
+    `earliest` so that in case the consumer group is being newly created, it will start
+    reading the messages from the beginning. If it is not set, it defaults to `latest`.
+    For additional configurations, please refer the librdkafka's configurations:
+    https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
 
     To train a keras model on this stream of incoming data:
 
@@ -75,13 +84,13 @@ class KafkaBatchIODataset(tf.data.Dataset):
           group_id: The id of the consumer group. For example: cgstream
           servers: An optional list of bootstrap servers.
             For example: `localhost:9092`.
-          stream_timeout: An optional timeout value (in milliseconds) to wait for 
+          stream_timeout: An optional timeout value (in milliseconds) to wait for
             the new messages from kafka to be retrieved by the consumers.
             By default it is set to -1 to block indefinitely.
           message_poll_timeout: An optional timeout duration (in milliseconds)
             after which the kafka consumer throws a timeout error while fetching
             a single message. This value also represents the intervals at which
-            the kafka topic(s) are polled for new messages while using the `stream_timeout`.  
+            the kafka topic(s) are polled for new messages while using the `stream_timeout`.
           configuration: An optional `tf.string` tensor containing
             configurations in [Key=Value] format.
             Global configuration: please refer to 'Global configuration properties'
@@ -89,8 +98,9 @@ class KafkaBatchIODataset(tf.data.Dataset):
               ["enable.auto.commit=false", "heartbeat.interval.ms=2000"]
             Topic configuration: please refer to 'Topic configuration properties'
               in librdkafka doc. Note all topic configurations should be
-              prefixed with `configuration.topic.`. Examples include
+              prefixed with `conf.topic.`. Examples include
               ["conf.topic.auto.offset.reset=earliest"]
+            Reference: https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
           internal: Whether the dataset is being created from within the named scope.
             Default: True
         """
