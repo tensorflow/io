acabd325a29952d6b709ff05f3ea7d65fbb52773
Vignesh Kothapalli <vikoth18@in.ibm.com>
2020-11-18 11:41:35 +0530

    [hdfs] register viewfs and har schemes

--- tensorflow_io/core/plugins/file_system_plugins.cc
+++ tensorflow_io/core/plugins/file_system_plugins.cc
@@ -18,7 +18,7 @@ limitations under the License.
 void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
   info->plugin_memory_allocate = tensorflow::io::plugin_memory_allocate;
   info->plugin_memory_free = tensorflow::io::plugin_memory_free;
-  info->num_schemes = 4;
+  info->num_schemes = 6;
   info->ops = static_cast<TF_FilesystemPluginOps*>(
       tensorflow::io::plugin_memory_allocate(info->num_schemes *
                                              sizeof(info->ops[0])));
@@ -26,4 +26,6 @@ void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
   tensorflow::io::http::ProvideFilesystemSupportFor(&info->ops[1], "http");
   tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[2], "s3e");
   tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[3], "hdfse");
+  tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "viewfse");
+  tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "hare");
 }
