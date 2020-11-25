b5e8932f70e9c3e96962a87fc1a87f8502b89109
Vignesh Kothapalli <vikoth18@in.ibm.com>
2020-11-26 00:16:47 +0530

    lint fixes

--- tensorflow_io/core/python/experimental/mongodb_dataset_ops.py
+++ tensorflow_io/core/python/experimental/mongodb_dataset_ops.py
@@ -35,9 +35,7 @@ class _MongoDBHandler:
         """Retrieve the resource which is connected to a healthy node"""
 
         resource = core_ops.io_mongo_readable_init(
-            uri=self.uri,
-            database=self.database,
-            collection=self.collection,
+            uri=self.uri, database=self.database, collection=self.collection,
         )
         print("Connection successful: {}".format(self.uri))
         return resource
