51180c87cf587464343b63649c99d727f695b799
Yong Tang <yong.tang.github@outlook.com>
2021-02-04 10:49:31 -0800

    Update names of api tests
    We renamed the tests to remove "_eager" parts. This PR updates the api test for correct filenames
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- .github/workflows/build.wheel.sh
+++ .github/workflows/build.wheel.sh
@@ -9,7 +9,7 @@ run_test() {
   $entry -m pip install -q pytest pytest-benchmark boto3 fastavro avro-python3 scikit-image pandas pyarrow==3.0.0 google-cloud-pubsub==2.1.0 google-cloud-bigtable==1.6.0 google-cloud-bigquery-storage==1.1.0 google-cloud-bigquery==2.3.1 google-cloud-storage==1.32.0
   (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append $(find . -type f \( -iname "test_*_v1.py" \)))
   (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append $(find . -type f \( -iname "test_*.py" ! \( -iname "test_*_v1.py" -o -iname "test_bigquery.py" \) \)))
-  # GRPC and test_bigquery_eager tests have to be executed separately because of https://github.com/grpc/grpc/issues/20034
+  # GRPC and test_bigquery tests have to be executed separately because of https://github.com/grpc/grpc/issues/20034
   (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append $(find . -type f \( -iname "test_bigquery.py" \)))
 }
 
