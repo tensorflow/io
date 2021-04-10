779f8c9a49dc481bab74aae2db0336c176fb0c0f
Yong Tang <yong.tang.github@outlook.com>
2021-04-10 10:36:15 -0700

    Update to use https for http file system test also registers https file system
    The http test is failing as apache switched to use https for license page.
    
    This PR makes adjustment to fix the issue.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/core/plugins/file_system_plugins.cc
+++ tensorflow_io/core/plugins/file_system_plugins.cc
@@ -23,24 +23,25 @@ void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
 
   info->plugin_memory_allocate = tensorflow::io::plugin_memory_allocate;
   info->plugin_memory_free = tensorflow::io::plugin_memory_free;
-  info->num_schemes = 7;
+  info->num_schemes = 8;
   info->ops = static_cast<TF_FilesystemPluginOps*>(
       tensorflow::io::plugin_memory_allocate(info->num_schemes *
                                              sizeof(info->ops[0])));
   tensorflow::io::az::ProvideFilesystemSupportFor(&info->ops[0], "az");
   tensorflow::io::http::ProvideFilesystemSupportFor(&info->ops[1], "http");
+  tensorflow::io::http::ProvideFilesystemSupportFor(&info->ops[2], "https");
   // Load plugins only when the environment variable is set
   if (load_plugin == "true" || load_plugin == "1") {
-    tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[2], "s3");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[3], "hdfs");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "viewfs");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "har");
-    tensorflow::io::gs::ProvideFilesystemSupportFor(&info->ops[6], "gs");
+    tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[3], "s3");
+    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "hdfs");
+    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "viewfs");
+    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[6], "har");
+    tensorflow::io::gs::ProvideFilesystemSupportFor(&info->ops[7], "gs");
   } else {
-    tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[2], "s3e");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[3], "hdfse");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "viewfse");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "hare");
-    tensorflow::io::gs::ProvideFilesystemSupportFor(&info->ops[6], "gse");
+    tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[3], "s3e");
+    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "hdfse");
+    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "viewfse");
+    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[6], "hare");
+    tensorflow::io::gs::ProvideFilesystemSupportFor(&info->ops[7], "gse");
   }
 }
