80eb4ae937b1cf658ca48c47c1ad1bcd77d3628e
Yong Tang <yong.tang.github@outlook.com>
2021-04-18 19:54:59 +0000

    Update add listener
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    Update
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    Update
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tests/test_kafka/kafka_test.sh
+++ tests/test_kafka/kafka_test.sh
@@ -22,6 +22,7 @@ VERSION=6.1.1
 echo "Downloading the confluent packages"
 curl -sSOL http://packages.confluent.io/archive/6.1/confluent-community-6.1.1.tar.gz
 tar -xzf confluent-community-6.1.1.tar.gz
+echo 'advertised.listeners=PLAINTEXT://127.0.0.1:9092' >> confluent-$VERSION/etc/kafka/server.properties
 
 (cd confluent-$VERSION/ && sudo bin/zookeeper-server-start -daemon etc/kafka/zookeeper.properties)
 echo "Waiting for 10 secs until zookeeper is up and running"
