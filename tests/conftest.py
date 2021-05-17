c36ab1fc860b94eb01c6fc28ac8eabe436a25bb8
Vignesh Kothapalli <vikoth18@in.ibm.com>
2021-05-17 16:39:53 +0530

    retain conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,6 +17,6 @@ conftest file for pytest
 """
 
 
-# def pytest_sessionfinish(session, exitstatus):
-#     if exitstatus == 5:
-#         session.exitstatus = 0
+def pytest_sessionfinish(session, exitstatus):
+    if exitstatus == 5:
+        session.exitstatus = 0
