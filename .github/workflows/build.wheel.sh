b5a6f5a1c49f8eab86b182eac84ee9f4f696c814
Vo Van Nghia <vovannghia2409@gmail.com>
2021-05-19 21:24:01 +0200

    run `test_filesystem` without `pytest_xdist` (#1430)
    * run `test_filesystem` without `pytest_xdist`
    
    * rename `test_filesystem` to `test_standalone_filesystem`

--- .github/workflows/build.wheel.sh
+++ .github/workflows/build.wheel.sh
@@ -9,7 +9,8 @@ run_test() {
   (cd wheelhouse && $entry -m pip install tensorflow_io_gcs_filesystem-*-cp${CPYTHON_VERSION}-*.whl)
   (cd wheelhouse && $entry -m pip install tensorflow_io-*-cp${CPYTHON_VERSION}-*.whl)
   $entry -m pip install -q pytest pytest-benchmark pytest-xdist boto3 fastavro avro-python3 scikit-image pandas pyarrow==3.0.0 google-cloud-pubsub==2.1.0 google-cloud-bigtable==1.6.0 google-cloud-bigquery-storage==1.1.0 google-cloud-bigquery==2.3.1 google-cloud-storage==1.32.0 PyYAML==5.3.1 azure-storage-blob==12.8.1
-  (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append --forked --numprocesses=auto --dist loadfile $(find . -type f \( -iname "test_*.py" \)))
+  (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append --forked --numprocesses=auto --dist loadfile $(find . -type f \( -iname "test_*.py" ! \( -iname "test_standalone_*.py" \) \)))
+  (cd tests && $entry -m pytest --benchmark-disable -v --import-mode=append $(find . -type f \( -iname "test_standalone_*.py" \)))
 }
 
 PYTHON_VERSION=python
