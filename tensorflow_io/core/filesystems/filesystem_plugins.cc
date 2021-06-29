c40207c1bcf3421b329160b64a991d08845d9e33
Yong Tang <yong.tang.github@outlook.com>
2021-06-29 16:12:06 -0700

    Remove TF_USE_MODULAR_FILESYSTEM for s3 and hdfs as they are not part of the tensorflow package anymore
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tensorflow_io/core/filesystems/filesystem_plugins.cc
+++ tensorflow_io/core/filesystems/filesystem_plugins.cc
@@ -27,9 +27,6 @@ limitations under the License.
 // tensorflow/tensorflow/c/experimental/filesystem/filesystem_interface.h
 // for definition of `TF_InitPlugin`
 TFIO_PLUGIN_EXPORT void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
-  const char* env_value = getenv("TF_USE_MODULAR_FILESYSTEM");
-  std::string load_plugin = env_value ? absl::AsciiStrToLower(env_value) : "";
-
   info->plugin_memory_allocate = tensorflow::io::plugin_memory_allocate;
   info->plugin_memory_free = tensorflow::io::plugin_memory_free;
   info->num_schemes = 7;
@@ -39,16 +36,8 @@ TFIO_PLUGIN_EXPORT void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
   tensorflow::io::az::ProvideFilesystemSupportFor(&info->ops[0], "az");
   tensorflow::io::http::ProvideFilesystemSupportFor(&info->ops[1], "http");
   tensorflow::io::http::ProvideFilesystemSupportFor(&info->ops[2], "https");
-  // Load plugins only when the environment variable is set
-  if (load_plugin == "true" || load_plugin == "1") {
-    tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[3], "s3");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "hdfs");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "viewfs");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[6], "har");
-  } else {
-    tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[3], "s3e");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "hdfse");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "viewfse");
-    tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[6], "hare");
-  }
+  tensorflow::io::s3::ProvideFilesystemSupportFor(&info->ops[3], "s3");
+  tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "hdfs");
+  tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "viewfs");
+  tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[6], "har");
 }
