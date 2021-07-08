2eb7021cb921380bde8c9305a6f97aa345e3010c
Vignesh Kothapalli <vikoth18@in.ibm.com>
2021-07-09 01:42:32 +0530

    revert conditional install of tensorflow-io-gcs-filesystem-nightly (#1470)
    start azurite in loose mode
    
    add missing pyarrow dependency
    
    add hadoop lib setup to test step

--- tests/test_azure/start_azure.sh
+++ tests/test_azure/start_azure.sh
@@ -4,6 +4,6 @@ set -o pipefail
 
 npm install azurite@3.12.0
 echo starting azurite-blob
-$(npm bin)/azurite-blob &
+$(npm bin)/azurite-blob --loose &
 sleep 10 # Wait for storage emulator to start
 echo azurite-blob started successfully
