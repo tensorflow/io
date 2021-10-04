ee584636179453c1239ccd666e667c5d16cd827c
janbernloehr <jan@bernloehrs.de>
2021-10-04 17:36:14 +0200

    Add support for bearer tokens

--- tensorflow_io/core/filesystems/az/az_filesystem.cc
+++ tensorflow_io/core/filesystems/az/az_filesystem.cc
@@ -190,6 +190,8 @@ std::shared_ptr<azure::storage_lite::storage_credential> get_credential(
                                                                         key);
   } else if (const auto sas = std::getenv("TF_AZURE_STORAGE_SAS")) {
     return std::make_shared<azure::storage_lite::shared_access_signature_credential>(sas);
+  } else if (const auto token = std::getenv("TF_AZURE_STORAGE_TOKEN")) {
+    return std::make_shared<azure::storage_lite::token_credential>(token);
   } else {
     return std::make_shared<azure::storage_lite::anonymous_credential>();
   }
