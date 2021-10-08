5d7cbd328375b4a7e1875adfe9eaab2df6f26f4f
Kajetan Boroszko <kajetan@unoperate.com>
2021-10-08 14:47:04 +0200

    a little prettier

--- tensorflow_io/core/kernels/bigtable/bigtable_kernels.cc
+++ tensorflow_io/core/kernels/bigtable/bigtable_kernels.cc
@@ -5,9 +5,9 @@ using namespace tensorflow;
 
 namespace cbt = ::google::cloud::bigtable;
 
-class ZeroOutOp : public OpKernel {
+class BigtableTestOp : public OpKernel {
 public:
-    explicit ZeroOutOp(OpKernelConstruction *context) : OpKernel(context) {
+    explicit BigtableTestOp(OpKernelConstruction *context) : OpKernel(context) {
     // Get the index of the value to preserve
     OP_REQUIRES_OK(context,
                    context->GetAttr("project_id", &project_id_));
@@ -17,22 +17,16 @@ public:
                    context->GetAttr("table_id", &table_id_));
     OP_REQUIRES_OK(context,
                    context->GetAttr("columns", &columns_));
+
+      int index = 0;
+      for(auto const& column : columns_){
+        column_map[column] = index++;
+      }
+
   }
 
 
     void Compute(OpKernelContext *context) override {
-
-
-      std::cout << "got " << project_id_ << ":" << instance_id_ << ":" << table_id_ << "\n";
-      std::cout << "columns len:" << columns_.size() << "\n";
-
-
-      std::map<std::string, int> column_map;
-      int counter = 0;
-      for(auto const& column : columns_){
-        column_map[column] = counter++;
-      }
-
       // Grab the input tensor
 
       cbt::Table table(cbt::CreateDefaultDataClient(project_id_, instance_id_,
@@ -50,16 +44,9 @@ public:
         std::vector<std::string> row_vec(column_map.size());
         std::fill(row_vec.begin(), row_vec.end(), "Nothing");
 
-        std::cout << " row: " << row->row_key() << ":\n";
         for (auto const &cell: row->cells()) {
-          std::cout << "cell:\n";
-          std::cout << cell.family_name() << ":" << cell.column_qualifier() << ":"
-                    << cell.value() << "       @ " << cell.timestamp().count()
-                    << "us\n";
           std::string col_name = cell.family_name() + ":" + cell.column_qualifier();
-          std::cout << "col_name:" << col_name << "\n";
           if(column_map.find(col_name) != column_map.end()){
-            std::cout << "found!\n";
             row_vec[column_map[col_name]] = cell.value();
           }
         }
@@ -75,8 +62,6 @@ public:
       // Set all but the first element of the output tensor to 0.
       const int N_rows = output_tensor->shape().dim_size(0);
       const int N_cols = output_tensor->shape().dim_size(1);
-      std::cout<< "N_rows:" << N_rows << "\n";
-      std::cout<< "N_cols:" << N_cols << "\n";
 
       for (int i = 0; i < N_rows; i++) {
         for(int j=0; j<N_cols; j++){
@@ -89,9 +74,7 @@ private:
     string instance_id_;
     string table_id_;
     std::vector<string> columns_;
+    std::map<std::string, int> column_map;
 };
 
-REGISTER_KERNEL_BUILDER(Name("BigtableTest")
-.
-Device(DEVICE_CPU), ZeroOutOp
-);
+REGISTER_KERNEL_BUILDER(Name("BigtableTest").Device(DEVICE_CPU), BigtableTestOp);
