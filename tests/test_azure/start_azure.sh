5bbc4c5a670676e397c5e7d24705085cb6be509c
janbernloehr <jan@bernloehrs.de>
2021-10-17 20:03:35 +0200

    Get SAS during test time

--- tests/test_azure/start_azure.sh
+++ tests/test_azure/start_azure.sh
@@ -7,3 +7,10 @@ echo starting azurite-blob
 $(npm bin)/azurite-blob --loose &
 sleep 10 # Wait for storage emulator to start
 echo azurite-blob started successfully
+
+path_to_az=$(which az)
+if [ -x "$path_to_az" ] ; then
+    echo "az is already installed: $path_to_az"
+else
+    npm install -g azure-cli  
+fi
