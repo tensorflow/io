6ca5b56c088d26e2362cba0fe8023018254892c4
janbernloehr <jan@bernloehrs.de>
2021-10-18 02:51:52 +0200

    Ensure env is setup

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -244,6 +244,15 @@ class AZFSSASTest(tf.test.TestCase, AZFSTestBase):
             .rstrip()
         )
 
+        env = os.environ.copy()
+        env["AZURE_STORAGE_CONNECTION_STRING"] = (
+            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
+            "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
+            "BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+            "QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
+            "TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
+        )
+
         os.environ["TF_AZURE_STORAGE_SAS"] = (
             subprocess.check_output(
                 [
@@ -262,7 +271,8 @@ class AZFSSASTest(tf.test.TestCase, AZFSTestBase):
                     sas_end,
                     "--account-name",
                     "devstoreaccount1",
-                ]
+                ],
+                env=env,
             )
             .decode()
             .rstrip()
