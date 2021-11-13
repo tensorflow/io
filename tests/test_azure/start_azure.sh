6b5bff706b301a2d6fef31e3e70285f880cc27fc
janbernloehr <jan@bernloehrs.de>
2021-11-13 15:54:49 +0100

    Update azurite to 3.14.3

--- tests/test_azure/start_azure.sh
+++ tests/test_azure/start_azure.sh
@@ -2,7 +2,7 @@ set -e
 set -o pipefail
 
 
-npm install azurite@3.12.0
+npm install azurite@3.14.3
 echo starting azurite-blob
 $(npm bin)/azurite-blob --loose &
 sleep 10 # Wait for storage emulator to start
