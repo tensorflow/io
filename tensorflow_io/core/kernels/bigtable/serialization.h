fb0981421dc55d18d9ef8238fe89ac4e7b5529f4
Kajetan Boroszko <kajetan@unoperate.com>
2021-11-26 14:52:58 +0100

    fix: building on windows (#12)

--- tensorflow_io/core/kernels/bigtable/serialization.h
+++ tensorflow_io/core/kernels/bigtable/serialization.h
@@ -18,15 +18,24 @@ limitations under the License.
 
 #include "google/cloud/bigtable/table.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/platform/statusor.h"
 
 namespace tensorflow {
 namespace io {
 
-// Bigtable only stores values as byte buffers - except for int64 the server
+// Bigtable only stores byte buffers as values - except for int64 the server
 // side does not have any notion of types. Tensorflow, needs to store shorter
-// integers, floats, doubles, so we needed to decide on how. We chose to follow
-// what HBase does, since there is a path for migrating from HBase to Bigtable.
-// XDR seems to match what HBase does.
+// integers, floats, doubles, so we needed to decide on how. We chose to
+// follow what HBase does, since there is a path for migrating from HBase to
+// Bigtable. HBase stores integers as big-endian and floats as IEEE754
+// (also big-endian). Given that integer endianness does not always match
+// float endianness, and the fact that there are architectures where it is
+// neither little nor big (BE-32), implementing this properly is non-trivial.
+// Ideally, we would use a library to do that. XDR matches what HBase does,
+// but it is not easily available on Windows, so we decided to go with a
+// hybrid approach. On Windows we assume that integer endianness matches float
+// endianness and implement the deserialization ourselves and everywhere else
+// we use XDR. For that reason we provide two implementations
 Status PutCellValueInTensor(Tensor& tensor, size_t index, DataType cell_type,
                             google::cloud::bigtable::Cell const& cell);
 
