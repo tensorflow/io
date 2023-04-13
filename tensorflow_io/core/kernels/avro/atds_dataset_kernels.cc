3e6c93c309485eecf0e7ac68944a70d34fcc352b
Lijuan Zhang <lijzhang@linkedin.com>
2023-04-13 15:29:50 +0000

    copyright to all files to avro/atds

--- tensorflow_io/core/kernels/avro/atds_dataset_kernels.cc
+++ tensorflow_io/core/kernels/avro/atds_dataset_kernels.cc
@@ -12,6 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+
 #include "tensorflow_io/core/kernels/avro/atds_dataset_kernels.h"
 
 #include <boost/iostreams/filter/gzip.hpp>
