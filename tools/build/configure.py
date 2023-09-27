cf7e16a9b997dd724c0463ebec2ff76e457b38fb
Yong Tang <yong.tang.github@outlook.com>
2023-09-27 08:24:40 -0700

    Update clang flags

--- tools/build/configure.py
+++ tools/build/configure.py
@@ -119,9 +119,9 @@ def write_config():
             bazel_rc.write("build --enable_platform_specific_config\n")
             # Needed for GRPC build
             bazel_rc.write('build:macos --copt="-DGRPC_BAZEL_BUILD"\n')
-            # Stay with 10.14 for macOS
-            bazel_rc.write('build:macos --copt="-mmacosx-version-min=10.14"\n')
-            bazel_rc.write('build:macos --linkopt="-mmacosx-version-min=10.14"\n')
+            # With macOS
+            bazel_rc.write('build:macos --copt="-target x86_64-apple-macosx12.1"\n')
+            bazel_rc.write('build:macos --linkopt="-target x86_64-apple-macosx12.1"\n')
             # Warns for unguarded uses of Objective-C APIs
             bazel_rc.write("build:macos --copt=-Wunguarded-availability\n")
             bazel_rc.write("build:macos --copt=-Wno-error=unused-but-set-variable\n")
