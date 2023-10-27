4b5bedefae4c69b67391e1f05c5e14167d4e20f6
Yong Tang <yong.tang.github@outlook.com>
2023-10-27 21:12:07 +0000

    Fix

--- .github/workflows/build.bazel.sh
+++ .github/workflows/build.bazel.sh
@@ -28,8 +28,8 @@ export PYTHON_BIN_PATH=`which $PYTHON`
 
 if [[ $(uname) == "Linux" ]]; then
   curl -sSOL https://github.com/bazelbuild/bazelisk/releases/download/v1.11.0/bazelisk-linux-amd64
-  echo mv bazelisk-linux-amd64 /usr/local/bin/bazel
-  echo chmod +x /usr/local/bin/bazel
+  sudo mv bazelisk-linux-amd64 /usr/local/bin/bazel
+  sudo chmod +x /usr/local/bin/bazel
 fi
 
 bazel version
