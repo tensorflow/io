0f866d8b1dd5189e340e05f918d783b828241513
Yong Tang <yong.tang.github@outlook.com>
2023-10-28 04:03:39 +0000

    Update

--- .github/workflows/build.bazel.sh
+++ .github/workflows/build.bazel.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-set -e -x
+set -x
 
 
 PYTHON=python3
