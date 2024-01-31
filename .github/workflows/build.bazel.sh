c54dd173bab4f351eeaff9d6e5d16a5a621b549e
Yong Tang <yong.tang.github@outlook.com>
2024-01-31 11:00:33 -0800

    Add placeholder to proceed with the github actions build on arm64

--- .github/workflows/build.bazel.sh
+++ .github/workflows/build.bazel.sh
@@ -60,7 +60,7 @@ elif [[ $(uname -m) == "arm64" && $(uname) == "Darwin" ]]; then
 bazel build \
   ${BAZEL_OPTIMIZATION} \
   -- //tensorflow_io_gcs_filesystem/... # //tensorflow_io/... # Try with filesystem only during testing
-
+  mkdir -p bazel-bin/tensorflow_io # Placeholder
 else
 
 bazel build \
