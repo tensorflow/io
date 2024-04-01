75be399b136e82973e9817ef2d4870fd0dd504b4
lipeidian <lipeidian@qq.com>
2024-04-01 19:16:15 +0800

    fix corrupt

--- tensorflow_io/core/filesystems/filesystem_plugins.cc
+++ tensorflow_io/core/filesystems/filesystem_plugins.cc
@@ -30,8 +30,9 @@ TFIO_PLUGIN_EXPORT void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
   info->plugin_memory_allocate = tensorflow::io::plugin_memory_allocate;
   info->plugin_memory_free = tensorflow::io::plugin_memory_free;
 
-#if !defined(_MSC_VER)
   info->num_schemes = 8;
+#if !defined(_MSC_VER)
+  info->num_schemes = 9;
 #endif
 
   info->ops = static_cast<TF_FilesystemPluginOps*>(
@@ -44,11 +45,9 @@ TFIO_PLUGIN_EXPORT void TF_InitPlugin(TF_FilesystemPluginInfo* info) {
   tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[4], "hdfs");
   tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[5], "viewfs");
   tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[6], "har");
-<<<<<<< HEAD
-#if !defined(_MSC_VER)
-  tensorflow::io::oss::ProvideFilesystemSupportFor(&info->ops[7], "oss");
-#endif
-=======
   tensorflow::io::hdfs::ProvideFilesystemSupportFor(&info->ops[7], "gvfs");
->>>>>>> feat(gvfs): support gvfs
+
+#if !defined(_MSC_VER)
+  tensorflow::io::oss::ProvideFilesystemSupportFor(&info->ops[8], "oss");
+#endif
 }
